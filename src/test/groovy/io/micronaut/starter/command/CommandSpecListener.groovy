1df3651b9de50a6558f0a516a80bb2a1bf71aa93
jameskleeh <james.kleeh@gmail.com>
2020-04-08 15:47:47 -0400

    Log output of failed command tests

---
+++ src/test/groovy/io/micronaut/starter/command/CommandSpecListener.groovy
@@ -0,0 +1,25 @@
+package io.micronaut.starter.command
+
+import org.spockframework.runtime.AbstractRunListener
+import org.spockframework.runtime.extension.AbstractGlobalExtension
+import org.spockframework.runtime.model.ErrorInfo
+import org.spockframework.runtime.model.SpecInfo
+
+class CommandSpecListener extends AbstractGlobalExtension {
+
+    @Override
+    void visitSpec(SpecInfo specInfo) {
+        specInfo.addSetupInterceptor(interceptor -> {
+            if (interceptor.instance instanceof CommandSpec) {
+                interceptor.spec.addListener(new AbstractRunListener() {
+                    @Override
+                    void error(ErrorInfo error) {
+                        System.err.println("Output from command:")
+                        System.err.println("--------------------------------------------------")
+                        System.err.println(((CommandSpec) interceptor.instance).baos.toString())
+                    }
+                })
+            }
+        })
+    }
+}
