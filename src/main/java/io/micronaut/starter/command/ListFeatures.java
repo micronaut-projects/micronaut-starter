37f7a9fe51af214000eb54a9d3c558f8a8372d0b
jameskleeh <james.kleeh@gmail.com>
2020-04-10 09:29:38 -0400

    Fix checkstyle

--- src/main/java/io/micronaut/starter/command/ListFeatures.java
+++ src/main/java/io/micronaut/starter/command/ListFeatures.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.starter.command;
 
 import io.micronaut.starter.ContextFactory;
@@ -50,7 +65,7 @@ public class ListFeatures {
         consoleOutput.out("Available Features");
         consoleOutput.out("@|blue (+)|@ denotes the feature is included by default");
         consoleOutput.out("  " + String.format("%1$-" + width + "s", "Name") + "Description");
-        consoleOutput.out("  " + new String(new char[width-2]).replace("\0", "-") + "  ---------------");
+        consoleOutput.out("  " + new String(new char[width - 2]).replace("\0", "-") + "  ---------------");
 
         for (Feature feature: allFeatures) {
             if (defaultFeatures.contains(feature)) {
