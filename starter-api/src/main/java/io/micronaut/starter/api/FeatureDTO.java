b08d339d8292632b242e7c49d1e74d337c7ad498
graemerocher <graeme.rocher@gmail.com>
2020-04-17 18:36:08 +0200

    Fix tests and add Lambda tests

--- starter-api/src/main/java/io/micronaut/starter/api/FeatureDTO.java
+++ starter-api/src/main/java/io/micronaut/starter/api/FeatureDTO.java
@@ -1,5 +1,6 @@
 package io.micronaut.starter.api;
 
+import io.micronaut.core.annotation.Creator;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.starter.feature.Feature;
 
@@ -11,27 +12,39 @@ import io.micronaut.starter.feature.Feature;
  */
 @Introspected
 public class FeatureDTO {
-    private final Feature feature;
-
+    private final String name;
+    private final String description;
     /**
      * Default constructor.
      * @param feature The feature
      */
     public FeatureDTO(Feature feature) {
-        this.feature = feature;
+        this.name = feature.getName();
+        this.description = feature.getDescription();
+    }
+
+    /**
+     * Default constructor.
+     * @param name The name
+     * @param description The description
+     */
+    @Creator
+    public FeatureDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 
     /**
      * @return The name of the feature
      */
     public String getName() {
-        return feature.getName();
+        return name;
     }
 
     /**
      * @return The description of the feature
      */
     public String getDescription() {
-        return feature.getDescription();
+        return description;
     }
 }
---
+++ starter-aws-lambda/src/test/groovy/io/micronaut/starter/lambda/FunctionSpec.groovy
@@ -0,0 +1,49 @@
+package io.micronaut.starter.lambda
+
+import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext
+import com.amazonaws.services.lambda.runtime.Context
+import io.micronaut.context.ApplicationContext
+import io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler
+import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpMethod
+import io.micronaut.http.HttpStatus
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+import java.util.zip.ZipInputStream
+
+class FunctionSpec extends Specification {
+    @Shared @AutoCleanup MicronautLambdaContainerHandler handler = new MicronautLambdaContainerHandler(
+            ApplicationContext.build()
+    )
+    @Shared Context lambdaContext = new MockLambdaContext()
+
+
+    void "test list features"() {
+
+        when:
+        AwsProxyRequestBuilder builder = new AwsProxyRequestBuilder('/features', HttpMethod.GET.toString())
+        def response = handler.proxy(builder.build(), lambdaContext)
+
+        then:
+        response.statusCode == HttpStatus.OK.code
+    }
+
+    void "test create app"() {
+
+        when:
+        AwsProxyRequestBuilder builder = new AwsProxyRequestBuilder('/create/app/test', HttpMethod.GET.toString())
+        def response = handler.proxy(builder.build(), lambdaContext)
+        def bytes = response.body.getBytes(StandardCharsets.UTF_8)
+        def zipInputStream = new ZipInputStream(new ByteArrayInputStream(bytes))
+        def zipEntry = zipInputStream.getNextEntry()
+
+        then:
+        response.statusCode == HttpStatus.CREATED.code
+        zipEntry != null
+        response.multiValueHeaders.getFirst(HttpHeaders.CONTENT_DISPOSITION).contains("application.zip")
+    }
+}
