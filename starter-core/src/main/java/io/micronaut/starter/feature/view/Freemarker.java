f2071acf8dce110052fe4a4e4585d045275a15dd
Iván López <lopezi@objectcomputing.com>
2020-04-21 13:32:38 +0200

    Add support for Freemarker feature
    Closes #83

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/view/Freemarker.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.view;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class Freemarker implements ViewFeature {
+
+    @Override
+    public String getName() {
+        return "views-freemarker";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Freemarker views";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for Apache Freemarker as a Server Side View Rendering (https://freemarker.apache.org)";
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/view/FreemarkerSpec.groovy
@@ -0,0 +1,48 @@
+package io.micronaut.starter.feature.view
+
+import io.micronaut.starter.BeanContextSpec
+import io.micronaut.starter.feature.build.gradle.templates.buildGradle
+import io.micronaut.starter.feature.build.maven.templates.pom
+import io.micronaut.starter.options.Language
+import spock.lang.Unroll
+
+class FreemarkerSpec extends BeanContextSpec {
+
+    @Unroll
+    void 'test gradle views-freemarker feature for language=#language'() {
+        when:
+        String template = buildGradle.template(buildProject(), getFeatures(['views-freemarker'], language)).render().toString()
+
+        then:
+        template.contains('implementation "io.micronaut:micronaut-views-freemarker"')
+        template.contains('runtime "org.freemarker:freemarker:2.3.28"')
+
+        where:
+        language << [Language.JAVA, Language.KOTLIN, Language.GROOVY]
+    }
+
+    @Unroll
+    void 'test maven views-freemarker feature for language=#language'() {
+        when:
+        String template = pom.template(buildProject(), getFeatures(['views-freemarker'], language), []).render().toString()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut</groupId>
+      <artifactId>micronaut-views-freemarker</artifactId>
+      <scope>compile</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.freemarker</groupId>
+      <artifactId>freemarker</artifactId>
+      <version>2.3.28</version>
+      <scope>runtime</scope>
+    </dependency>
+""")
+
+        where:
+        language << [Language.JAVA, Language.KOTLIN, Language.GROOVY]
+    }
+
+}
