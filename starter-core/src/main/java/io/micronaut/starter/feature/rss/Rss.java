1bd899f6beec83e19f8d54a1e5a963bd9d2079c6
Iván López <lopezi@objectcomputing.com>
2020-04-21 15:58:23 +0200

    Add support for rss and iTunes podcast rss
    Closes #84

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/rss/Rss.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.rss;
+
+import io.micronaut.starter.feature.Feature;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class Rss implements Feature {
+
+    @Override
+    public String getName() {
+        return "rss";
+    }
+
+    @Override
+    public String getTitle() {
+        return "RSS Feed";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for generating RSS feeds (https://cyber.harvard.edu/rss/rss.html)";
+    }
+
+}
