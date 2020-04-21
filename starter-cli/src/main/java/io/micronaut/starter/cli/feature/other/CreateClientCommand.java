0e1917e9129e8d7c31ac087ecf25db25a5876543
jameskleeh <james.kleeh@gmail.com>
2020-04-21 17:15:46 -0400

    Add create-client command

---
+++ starter-cli/src/main/java/io/micronaut/starter/cli/feature/other/CreateClientCommand.java
@@ -0,0 +1,67 @@
+package io.micronaut.starter.cli.feature.other;
+
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.core.util.functional.ThrowingSupplier;
+import io.micronaut.starter.application.Project;
+import io.micronaut.starter.cli.CodeGenConfig;
+import io.micronaut.starter.cli.command.CodeGenCommand;
+import io.micronaut.starter.io.ConsoleOutput;
+import io.micronaut.starter.io.OutputHandler;
+import io.micronaut.starter.options.Language;
+import io.micronaut.starter.template.RenderResult;
+import io.micronaut.starter.template.RockerTemplate;
+import io.micronaut.starter.template.TemplateRenderer;
+import picocli.CommandLine;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@CommandLine.Command(name = "create-client", description = "Creates a client interface")
+public class CreateClientCommand extends CodeGenCommand {
+
+    @CommandLine.Parameters(paramLabel = "CLIENT-NAME", description = "The name of the client to create")
+    String clientName;
+
+    @Inject
+    public CreateClientCommand(@Parameter CodeGenConfig config) {
+        super(config);
+    }
+
+    public CreateClientCommand(CodeGenConfig config, ThrowingSupplier<OutputHandler, IOException> outputHandlerSupplier, ConsoleOutput consoleOutput) {
+        super(config, outputHandlerSupplier, consoleOutput);
+    }
+
+    @Override
+    public boolean applies() {
+        return config.getFeatures().contains("http-client");
+    }
+
+    @Override
+    public Integer call() throws Exception {
+
+        Project project = getProject(clientName);
+
+        TemplateRenderer templateRenderer = getTemplateRenderer(project);
+
+        RenderResult renderResult = null;
+        if (config.getSourceLanguage() == Language.JAVA) {
+            renderResult = templateRenderer.render(new RockerTemplate("src/main/java/{packagePath}/{className}Client.java", javaClient.template(project)), overwrite);
+        } else if (config.getSourceLanguage() == Language.GROOVY) {
+            renderResult = templateRenderer.render(new RockerTemplate("src/main/groovy/{packagePath}/{className}Client.groovy", groovyClient.template(project)), overwrite);
+        } else if (config.getSourceLanguage() == Language.KOTLIN) {
+            renderResult = templateRenderer.render(new RockerTemplate("src/main/kotlin/{packagePath}/{className}Client.kt", kotlinClient.template(project)), overwrite);
+        }
+
+        if (renderResult != null) {
+            if (renderResult.isSuccess()) {
+                out("@|blue ||@ Rendered client to " + renderResult.getPath());
+            } else if (renderResult.isSkipped()) {
+                warning("Rendering skipped for " + renderResult.getPath() + " because it already exists. Run again with -f to overwrite.");
+            } else if (renderResult.getError() != null) {
+                throw renderResult.getError();
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ starter-cli/src/test/groovy/io/micronaut/starter/cli/feature/other/CreateClientSpec.groovy
@@ -0,0 +1,45 @@
+package io.micronaut.starter.cli.feature.other
+
+import io.micronaut.context.BeanContext
+import io.micronaut.starter.cli.CodeGenConfig
+import io.micronaut.starter.cli.CommandFixture
+import io.micronaut.starter.cli.CommandSpec
+import io.micronaut.starter.cli.feature.database.CreateRepositoryCommand
+import io.micronaut.starter.io.ConsoleOutput
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Unroll
+
+class CreateClientSpec extends CommandSpec implements CommandFixture {
+
+    @Shared
+    @AutoCleanup
+    BeanContext beanContext = BeanContext.run()
+
+    @Unroll
+    void "test creating a client - #language.getName()"() {
+        generateDefaultProject(language, BuildTool.GRADLE)
+        CodeGenConfig codeGenConfig = CodeGenConfig.load(beanContext, dir, ConsoleOutput.NOOP)
+        ConsoleOutput consoleOutput = Mock(ConsoleOutput)
+        CreateClientCommand command = new CreateClientCommand(codeGenConfig, getOutputHandler(consoleOutput), consoleOutput)
+        command.clientName = "User"
+
+        when:
+        Integer exitCode = command.call()
+        File output = new File(dir, file)
+
+        then:
+        exitCode == 0
+        output.exists()
+        1 * consoleOutput.out({ it.contains("Rendered client") })
+
+        where:
+        language        | file
+        Language.JAVA   | "src/main/java/example/micronaut/UserClient.java"
+        Language.KOTLIN | "src/main/kotlin/example/micronaut/UserClient.kt"
+        Language.GROOVY | "src/main/groovy/example/micronaut/UserClient.groovy"
+    }
+
+}
