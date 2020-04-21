b53387e6c63774e379c8dcec3b737d6087505cd8
graemerocher <graeme.rocher@gmail.com>
2020-04-21 07:54:42 +0200

    Use local tmp directory

--- install-graal.sh
+++ install-graal.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 GRAAL_FILENAME="graalvm-ce-java8-${GRAAL_OS}-${GRAAL_VERSION}.tar.gz"
 
-curl -4 -L https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAAL_VERSION}/${GRAAL_FILENAME} -o /tmp/${GRAAL_FILENAME}
-tar -zxvf /tmp/${GRAAL_FILENAME} -C /tmp && mv /tmp/graalvm-ce-java8-${GRAAL_VERSION} /usr/lib/graalvm
-rm -rf /tmp/*
+mkdir tmp
+curl -4 -L https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAAL_VERSION}/${GRAAL_FILENAME} -o tmp/${GRAAL_FILENAME}
+tar -zxvf tmp/${GRAAL_FILENAME} -C tmp && mv tmp/graalvm-ce-java8-${GRAAL_VERSION} graalvm
+rm -rf tmp/*
