533566ca3237b98dad2be2defcaf5a026e33f5ed
graemerocher <graeme.rocher@gmail.com>
2020-04-22 12:37:39 +0200

    Fix kotlin generation

--- starter-core/src/main/java/io/micronaut/starter/feature/function/gcp/GoogleCloudFunction.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/function/gcp/GoogleCloudFunction.java
@@ -24,6 +24,7 @@ import io.micronaut.starter.feature.function.FunctionFeature;
 import io.micronaut.starter.feature.function.gcp.template.*;
 import io.micronaut.starter.feature.server.template.groovyController;
 import io.micronaut.starter.feature.server.template.javaController;
+import io.micronaut.starter.feature.server.template.kotlinController;
 import io.micronaut.starter.options.Language;
 import io.micronaut.starter.options.TestFramework;
 import io.micronaut.starter.template.RockerTemplate;
@@ -62,30 +63,31 @@ public class GoogleCloudFunction implements FunctionFeature {
         ApplicationType type = generatorContext.getApplicationType();
         if (type == ApplicationType.DEFAULT) {
             Project project = generatorContext.getProject().withClassName("Hello");
-
             Language language = generatorContext.getLanguage();
+            String sourceFile = language.getSrcDir() + "/{packagePath}/HelloController." + language.getExtension();
+            TestFramework testFramework = generatorContext.getTestFramework();
+            String testSource =  testFramework.getFilename("/{packagePath}/HelloFunction", language);
             switch (language) {
                 case GROOVY:
                     generatorContext.addTemplate("function", new RockerTemplate(
-                            "src/main/groovy/{packagePath}/HelloController.groovy",
+                            sourceFile,
                             groovyController.template(project)));
                     break;
                 case KOTLIN:
                     generatorContext.addTemplate("function", new RockerTemplate(
-                            "src/main/kotlin/{packagePath}/HelloController.kt",
-                            groovyController.template(project)));
+                            sourceFile,
+                            kotlinController.template(project)));
                     break;
                 case JAVA:
                 default:
                     generatorContext.addTemplate("function", new RockerTemplate(
-                            "src/main/java/{packagePath}/HelloController.java",
+                            sourceFile,
                             javaController.template(project)));
                     break;
             }
 
-            TestFramework testFramework = generatorContext.getTestFramework();
-            String testFile = language.getTestSrcDir() + "/{packagePath}/HelloFunctionTest." + language.getExtension();
-            RockerModel testTemplate = null;
+
+            RockerModel testTemplate;
             switch (testFramework) {
                 case SPOCK:
                     testTemplate = gcpFunctionSpock.template(project);
@@ -112,7 +114,7 @@ public class GoogleCloudFunction implements FunctionFeature {
 
             if (testTemplate != null) {
                 generatorContext.addTemplate("testFunction", new RockerTemplate(
-                        testFile,
+                        testSource,
                         testTemplate)
                 );
             }
