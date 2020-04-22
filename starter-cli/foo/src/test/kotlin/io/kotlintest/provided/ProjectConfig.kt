2a47f0ae0b8841ca3879fd138ec776631fb3c969
jameskleeh <james.kleeh@gmail.com>
2020-04-21 21:19:21 -0400

    Delete accidentally pushed test project

--- starter-cli/foo/src/test/kotlin/io/kotlintest/provided/ProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-package kotlintest
-
-import io.kotlintest.AbstractProjectConfig
-import io.micronaut.test.extensions.kotlintest.MicronautKotlinTestExtension
-
-object ProjectConfig : AbstractProjectConfig() {
-    override fun listeners() = listOf(MicronautKotlinTestExtension)
-    override fun extensions() = listOf(MicronautKotlinTestExtension)
-}
