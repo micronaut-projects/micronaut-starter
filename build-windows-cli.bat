ee3cd338ea55dec2fc1e578e85d78d5546bdfabc
graemerocher <graeme.rocher@gmail.com>
2020-04-22 18:54:04 +0200

    Take 37

--- build-windows-cli.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-echo "Building JAR File"
-./gradlew starter-cli:copyShadowJar
-native-image --no-server --no-fallback -cp build/libs/cli.jar
