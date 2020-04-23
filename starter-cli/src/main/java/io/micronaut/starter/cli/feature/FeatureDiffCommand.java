f6a45156e6ddb0f5970cf496bb75df7319a39a27
jameskleeh <james.kleeh@gmail.com>
2020-04-23 00:37:14 -0400

    Fix checkstyle

--- starter-cli/src/main/java/io/micronaut/starter/cli/feature/FeatureDiffCommand.java
+++ starter-cli/src/main/java/io/micronaut/starter/cli/feature/FeatureDiffCommand.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.starter.cli.feature;
 
 import com.github.difflib.DiffUtils;
@@ -25,11 +40,11 @@ import java.util.*;
 @CommandLine.Command(name = "feature-diff", description = "Produces the diff of an original project with an original project with additional features.")
 public class FeatureDiffCommand extends CodeGenCommand {
 
-    private final ProjectGenerator projectGenerator;
-
     @ReflectiveAccess
     @CommandLine.Option(names = {"--features"}, paramLabel = "FEATURE", split = ",", description = "The additional features")
     List<String> features = new ArrayList<>();
+
+    private final ProjectGenerator projectGenerator;
 
     @Inject
     public FeatureDiffCommand(@Parameter CodeGenConfig config, ProjectGenerator projectGenerator) {
