a7dd308831382ee343f9e22fe39f6d80c6c09a52
Sergio del Amo <sergio.delamo@softamo.com>
2020-04-23 17:41:24 +0200

    feat: dependencies for AWS API Gateway Lambda proxy
    see: https://github.com/micronaut-projects/micronaut-starter/issues/123

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/awsapiproxy/AwsApiGatewayLambdaProxy.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.awsapiproxy;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.feature.Feature;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class AwsApiGatewayLambdaProxy implements Feature {
+    @NonNull
+    @Override
+    public String getName() {
+        return "aws-api-gateway-lambda-proxy";
+    }
+
+    public String getTitle() {
+        return "AWS API Gateway Lambda Proxy";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Deploy your application to AWS Lambda and use API Gateway to proxy requests to it. Code your application with Controllers.";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return applicationType == ApplicationType.DEFAULT;
+    }
+}
