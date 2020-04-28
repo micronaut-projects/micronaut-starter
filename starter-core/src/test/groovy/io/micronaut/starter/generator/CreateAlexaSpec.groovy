c33ee830ce084c79098fd84fcd5584e81de44a98
Sergio del Amo <sergio.delamo@softamo.com>
2020-04-28 09:39:56 +0200

    fix: ignore them faster than pending feature
    PendingFeature executes them

--- starter-core/src/test/groovy/io/micronaut/starter/generator/CreateAlexaSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/generator/CreateAlexaSpec.groovy
@@ -5,12 +5,11 @@ import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.options.TestFramework
 import spock.lang.Ignore
-import spock.lang.PendingFeature
 import spock.lang.Unroll
 
 class CreateAlexaSpec  extends CommandSpec {
 
-    @PendingFeature(reason = "alexa-httpserver not resolved")
+    @Ignore("alexa-httpserver not resolved")
     @Unroll
     void 'create-app with features #features #lang and #build and test framework: #testFramework'(ApplicationType applicationType,
                                                                                                   List<String> features,
