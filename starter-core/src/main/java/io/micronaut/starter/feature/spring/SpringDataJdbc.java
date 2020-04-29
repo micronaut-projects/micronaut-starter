43c4715dddbb02dfa5634f57ec52943111dafd28
Iván López <lopezi@objectcomputing.com>
2020-04-29 19:48:38 +0200

    Add Spring Data JDBC feature
    Closes #170

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/spring/SpringDataJdbc.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.spring;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.feature.FeatureContext;
+import io.micronaut.starter.feature.database.DataJdbc;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class SpringDataJdbc extends SpringFeature {
+
+    private final DataJdbc dataJdbc;
+
+    public SpringDataJdbc(Spring spring, DataJdbc dataJdbc) {
+        super(spring);
+        this.dataJdbc = dataJdbc;
+    }
+
+    @Override
+    public String getName() {
+        return "spring-data-jdbc";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Spring Data JDBC Annotations";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for using Spring Data JDBC Annotations";
+    }
+
+    @Override
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        super.processSelectedFeatures(featureContext);
+        if (!featureContext.isPresent(DataJdbc.class)) {
+            featureContext.addFeature(dataJdbc);
+        }
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/spring/SpringDataJdbcSpec.groovy
@@ -0,0 +1,91 @@
+package io.micronaut.starter.feature.spring
+
+import io.micronaut.starter.BeanContextSpec
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.feature.Features
+import io.micronaut.starter.feature.build.gradle.templates.buildGradle
+import io.micronaut.starter.feature.build.maven.templates.pom
+import io.micronaut.starter.options.Language
+import spock.lang.Shared
+import spock.lang.Subject
+import spock.lang.Unroll
+
+class SpringDataJdbcSpec extends BeanContextSpec {
+
+    @Shared
+    @Subject
+    SpringDataJdbc springDataJdbc = beanContext.getBean(SpringDataJdbc)
+
+    void 'spring-data-jdbc belongs to Spring category'() {
+        expect:
+        Category.SPRING == springDataJdbc.category
+    }
+
+    void 'spring-data-jdbc is visible'() {
+        expect:
+        springDataJdbc.visible
+    }
+
+    void 'spring-data-jdbc title and description are different'() {
+        expect:
+        springDataJdbc.getTitle()
+        springDataJdbc.getDescription()
+        springDataJdbc.getTitle() != springDataJdbc.getDescription()
+    }
+
+    @Unroll
+    void 'feature spring-data-jdbc supports every type of application type. applicationType=#applicationType'() {
+        expect:
+        springDataJdbc.supports(applicationType)
+
+        where:
+        applicationType << ApplicationType.values().toList()
+    }
+
+    void 'test spring-data-jdbc features'() {
+        when:
+        Features features = getFeatures(['spring-data-jdbc'])
+
+        then:
+        features.contains('data-jdbc')
+        features.contains('spring')
+    }
+
+    @Unroll
+    void 'test spring-data-jdbc with Gradle for language=#language'() {
+        when:
+        String template = buildGradle.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['spring-data-jdbc'], language)).render().toString()
+
+        then:
+        template.contains('implementation("io.micronaut.data:micronaut-data-spring")')
+        template.contains('implementation("org.springframework:spring-jdbc")')
+
+        where:
+        language << Language.values().toList()
+    }
+
+    @Unroll
+    void 'test maven spring-data-jdbc feature for language=#language'() {
+        when:
+        String template = pom.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['spring-data-jdbc'], language), []).render().toString()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.data</groupId>
+      <artifactId>micronaut-data-spring</artifactId>
+      <scope>compile</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.springframework</groupId>
+      <artifactId>spring-jdbc</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+
+        where:
+        language << Language.values().toList()
+    }
+
+}
