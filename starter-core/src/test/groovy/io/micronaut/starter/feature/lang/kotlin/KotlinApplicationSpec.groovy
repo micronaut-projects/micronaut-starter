916227ed62a1c84fef0652fdc1e295300f4ba061
Sergio del Amo <sergio.delamo@softamo.com>
2020-04-29 13:29:56 +0200

    fix: include kotlin-runtime from Kotlin
    Close: https://github.com/micronaut-projects/micronaut-starter/issues/159

--- starter-core/src/test/groovy/io/micronaut/starter/feature/lang/kotlin/KotlinApplicationSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/lang/kotlin/KotlinApplicationSpec.groovy
@@ -7,6 +7,7 @@ import io.micronaut.starter.options.Language
 import io.micronaut.starter.options.Options
 import io.micronaut.starter.options.TestFramework
 import io.micronaut.starter.fixture.CommandOutputFixture
+import spock.lang.Unroll
 
 class KotlinApplicationSpec extends BeanContextSpec implements CommandOutputFixture {
     void 'Application file is generated for a default application type with gradle and referenced in build.gradle mainClassName for language: kotlin'() {
@@ -25,5 +26,66 @@ class KotlinApplicationSpec extends BeanContextSpec implements CommandOutputFixt
 
         then:
         buildGradle.contains('mainClassName = "example.micronaut.Application"')
+        buildGradle.contains('implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")')
+    }
+
+    @Unroll
+    void "for gradle and #language kotlin-runtime is not added"() {
+        when:
+        def output = generate(
+                ApplicationType.DEFAULT,
+                new Options(language, null, BuildTool.GRADLE),
+                []
+        )
+        def buildGradle = output['build.gradle']
+
+        then:
+        !buildGradle.contains('implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")')
+
+        where:
+        language << (Language.values().toList() - Language.KOTLIN)
+    }
+
+    @Unroll
+    void "for maven and #language kotlin-runtime is not added"() {
+        when:
+        def output = generate(
+                ApplicationType.DEFAULT,
+                new Options(language, null, BuildTool.MAVEN),
+                []
+        )
+        def pom = output['pom.xml']
+
+        then:
+        !pom.contains("""
+    <dependency>
+      <groupId>io.micronaut.kotlin</groupId>
+      <artifactId>micronaut-kotlin-runtime</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+
+        where:
+        language << (Language.values().toList() - Language.KOTLIN)
+    }
+
+    @Unroll
+    void "for maven and kotlin kotlin-runtime is added"() {
+        when:
+        def output = generate(
+                ApplicationType.DEFAULT,
+                new Options(Language.KOTLIN, null, BuildTool.MAVEN),
+                []
+        )
+        def pom = output['pom.xml']
+
+        then:
+        pom.contains("""
+    <dependency>
+      <groupId>io.micronaut.kotlin</groupId>
+      <artifactId>micronaut-kotlin-runtime</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
     }
 }
