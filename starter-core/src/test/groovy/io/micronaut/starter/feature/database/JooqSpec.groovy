0a83973c0d1c2dc0a1c042f4c395264502561de3
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-04-29 12:19:16 -0700

    Add jOOQ formula (#153)
    * Add jOOQ feature

---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/database/JooqSpec.groovy
@@ -0,0 +1,42 @@
+package io.micronaut.starter.feature.database
+
+import io.micronaut.starter.BeanContextSpec
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.feature.build.gradle.templates.buildGradle
+import io.micronaut.starter.feature.build.maven.templates.pom
+import io.micronaut.starter.options.Language
+import spock.lang.Unroll
+
+class JooqSpec extends BeanContextSpec {
+
+    @Unroll
+    void 'test gradle jooq feature for language=#language'() {
+        when:
+        String template = buildGradle.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['jooq'], language)).render().toString()
+
+        then:
+        template.contains('implementation("io.micronaut.configuration:micronaut-jooq")')
+
+        where:
+        language << Language.values().toList()
+    }
+
+    @Unroll
+    void 'test maven jooq feature for language=#language'() {
+        when:
+        String template = pom.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['jooq'], language), []).render().toString()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.configuration</groupId>
+      <artifactId>micronaut-jooq</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+
+        where:
+        language << Language.values().toList()
+    }
+
+}
