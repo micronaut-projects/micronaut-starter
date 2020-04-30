6c7077dd3821becccf1212ea08b3d28627b56bf9
jameskleeh <james.kleeh@gmail.com>
2020-04-30 01:37:16 -0400

    Support readme feature

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/other/Readme.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.other;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.feature.DefaultFeature;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.feature.FeaturePhase;
+import io.micronaut.starter.options.Options;
+import io.micronaut.starter.template.Template;
+import io.micronaut.starter.template.Writable;
+
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+
+@Singleton
+public class Readme implements DefaultFeature {
+
+    @Override
+    public boolean shouldApply(ApplicationType applicationType, Options options, Set<Feature> selectedFeatures) {
+        return true;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "readme";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        List<Writable> helpTemplates = generatorContext.getHelpTemplates();
+        if (helpTemplates.size() > 0) {
+            generatorContext.addTemplate("readme", new Template() {
+                @Override
+                public String getPath() {
+                    return "README.md";
+                }
+
+                @Override
+                public void write(OutputStream outputStream) throws IOException {
+                    for (Writable writable : generatorContext.getHelpTemplates()) {
+                        writable.write(outputStream);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return FeaturePhase.HIGHEST.getOrder();
+    }
+}
