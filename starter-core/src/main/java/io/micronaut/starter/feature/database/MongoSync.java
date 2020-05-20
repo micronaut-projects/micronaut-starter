8e0cc8f467496e74de4c77aeac3e111f47c21218
graemerocher <graeme.rocher@gmail.com>
2020-05-20 11:20:26 +0200

    Update MongoDB feature

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/database/MongoSync.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.database;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class MongoSync implements Feature {
+
+    @Override
+    public String getName() {
+        return "mongo-sync";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Mongo Synchronous Driver";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for the Mongo Synchronous Driver";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.getConfiguration().put("mongodb.uri", "mongodb://${MONGO_HOST:localhost}:${MONGO_PORT:27017}");
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.DATABASE;
+    }
+
+}
--- starter-core/src/test/groovy/io/micronaut/starter/feature/database/MongoSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/database/MongoSpec.groovy
@@ -7,7 +7,46 @@ import io.micronaut.starter.feature.Features
 import io.micronaut.starter.feature.build.gradle.templates.buildGradle
 import io.micronaut.starter.feature.build.maven.templates.pom
 
-class MongoReactiveSpec extends BeanContextSpec {
+class MongoSpec extends BeanContextSpec {
+
+    void "test mongo sync features"() {
+        when:
+        Features features = getFeatures(['mongo-sync'])
+
+        then:
+        features.contains("mongo-sync")
+    }
+
+    void "test mongo sync dependencies are present for gradle"() {
+        when:
+        String template = buildGradle.template(ApplicationType.DEFAULT, buildProject(), getFeatures(["mongo-sync"])).render().toString()
+
+        then:
+        template.contains('implementation("io.micronaut.mongodb:micronaut-mongo-sync")')
+        template.contains("testImplementation(\"de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.0.1\")")
+    }
+
+    void "test mongo sync dependencies are present for maven"() {
+        when:
+        String template = pom.template(ApplicationType.DEFAULT, buildProject(), getFeatures(["mongo-sync"]), []).render().toString()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.mongodb</groupId>
+      <artifactId>micronaut-mongo-sync</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+        template.contains("""
+    <dependency>
+      <groupId>de.flapdoodle.embed</groupId>
+      <artifactId>de.flapdoodle.embed.mongo</artifactId>
+      <version>2.0.1</version>
+      <scope>test</scope>
+    </dependency>
+""")
+    }
 
     void "test mongo reactive features"() {
         when:
@@ -22,7 +61,7 @@ class MongoReactiveSpec extends BeanContextSpec {
         String template = buildGradle.template(ApplicationType.DEFAULT, buildProject(), getFeatures(["mongo-reactive"])).render().toString()
 
         then:
-        template.contains('implementation("io.micronaut.configuration:micronaut-mongo-reactive")')
+        template.contains('implementation("io.micronaut.mongodb:micronaut-mongo-reactive")')
         template.contains("testImplementation(\"de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.0.1\")")
     }
 
@@ -33,7 +72,7 @@ class MongoReactiveSpec extends BeanContextSpec {
         then:
         template.contains("""
     <dependency>
-      <groupId>io.micronaut.configuration</groupId>
+      <groupId>io.micronaut.mongodb</groupId>
       <artifactId>micronaut-mongo-reactive</artifactId>
       <scope>compile</scope>
     </dependency>
