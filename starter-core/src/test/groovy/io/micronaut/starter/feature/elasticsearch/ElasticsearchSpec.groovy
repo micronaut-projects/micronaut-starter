4f95496f8f9597832c0fbec3853e5f64b5e9e2ed
Iván López <lopezi@objectcomputing.com>
2020-06-17 13:11:44 +0200

    Add logging dependencies for elasticsearch if graalvm selected (#282)

--- starter-core/src/test/groovy/io/micronaut/starter/feature/elasticsearch/ElasticsearchSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/elasticsearch/ElasticsearchSpec.groovy
@@ -7,6 +7,7 @@ import io.micronaut.starter.feature.build.gradle.templates.buildGradle
 import io.micronaut.starter.feature.build.maven.templates.pom
 import io.micronaut.starter.fixture.CommandOutputFixture
 import io.micronaut.starter.options.Language
+import spock.lang.Requires
 import spock.lang.Unroll
 
 class ElasticsearchSpec extends BeanContextSpec  implements CommandOutputFixture {
@@ -57,5 +58,56 @@ class ElasticsearchSpec extends BeanContextSpec  implements CommandOutputFixture
 
         then:
         commandContext.configuration.get('elasticsearch.httpHosts'.toString()) == '"http://localhost:9200,http://127.0.0.2:9200"'
+    }
+
+    @Unroll
+    @Requires({ jvm.isJava8() || jvm.isJava11() })
+    void 'test gradle elasticsearch and graalvm features for language=#language'() {
+        when:
+        String template = buildGradle.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['elasticsearch', 'graalvm'], language)).render().toString()
+
+        then:
+        template.contains('runtimeOnly("org.slf4j:log4j-over-slf4j:1.7.30")')
+        template.contains('implementation("org.apache.logging.log4j:log4j-api:2.13.3")')
+        template.contains('implementation("org.apache.logging.log4j:log4j-core:2.13.3")')
+
+        where:
+        language << Language.values().toList() - Language.GROOVY
+    }
+
+    @Unroll
+    @Requires({ jvm.isJava8() || jvm.isJava11() })
+    void 'test maven elasticsearch and graalvm features for language=#language'() {
+        when:
+        String template = pom.template(ApplicationType.DEFAULT, buildProject(), getFeatures(['elasticsearch', 'graalvm'], language), []).render().toString()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>org.slf4j</groupId>
+      <artifactId>log4j-over-slf4j</artifactId>
+      <version>1.7.30</version>
+      <scope>runtime</scope>
+    </dependency>
+""")
+        template.contains("""
+    <dependency>
+      <groupId>org.apache.logging.log4j</groupId>
+      <artifactId>log4j-api</artifactId>
+      <version>2.13.3</version>
+      <scope>compile</scope>
+    </dependency>
+""")
+        template.contains("""
+    <dependency>
+      <groupId>org.apache.logging.log4j</groupId>
+      <artifactId>log4j-core</artifactId>
+      <version>2.13.3</version>
+      <scope>compile</scope>
+    </dependency>
+""")
+
+        where:
+        language << Language.values().toList() - Language.GROOVY
     }
 }
