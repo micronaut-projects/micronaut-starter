c59f375f2fa5fc65348c572e9c35aa64dc69b46d
Alejandro Pérez García <alejandropg@autentia.com>
2020-07-12 06:55:02 +0200

    Remove unused imports to fix Checkstyle

--- starter-core/src/main/java/io/micronaut/starter/feature/test/TestFeature.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/test/TestFeature.java
@@ -17,11 +17,9 @@ package io.micronaut.starter.feature.test;
 
 import io.micronaut.starter.application.ApplicationType;
 import io.micronaut.starter.application.generator.GeneratorContext;
-import io.micronaut.starter.build.BuildProperties;
 import io.micronaut.starter.feature.DefaultFeature;
 import io.micronaut.starter.feature.Feature;
 import io.micronaut.starter.feature.FeaturePhase;
-import io.micronaut.starter.options.BuildTool;
 import io.micronaut.starter.options.Language;
 import io.micronaut.starter.options.Options;
 import io.micronaut.starter.options.TestFramework;
