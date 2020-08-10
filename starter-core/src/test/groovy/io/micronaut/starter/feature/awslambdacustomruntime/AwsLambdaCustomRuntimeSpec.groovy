ff81a0f6e67444c169844bf3a60597de6cffa791
Iván López <lopezi@objectcomputing.com>
2020-08-10 18:01:49 +0200

    Increase memory for graalvm native deployed to lambda. (#379)
    * Increase memory for graalvm native deployed to lambda.
    
    Fixes #322
    
    * revert java.library.path
    
    * revert java.library.path
    
    * revert java.library.path
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- starter-core/src/test/groovy/io/micronaut/starter/feature/awslambdacustomruntime/AwsLambdaCustomRuntimeSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/awslambdacustomruntime/AwsLambdaCustomRuntimeSpec.groovy
@@ -83,7 +83,7 @@ class AwsLambdaCustomRuntimeSpec extends BeanContextSpec  implements CommandOutp
         then:
         bootstrap.contains('#!/bin/sh')
         bootstrap.contains('set -euo pipefail')
-        bootstrap.contains('./foo -Xmx128m -Djava.library.path=$(pwd)')
+        bootstrap.contains('./foo -Xmx512m -Djava.library.path=$(pwd)')
 
         where:
         language << graalSupportedLanguages()
@@ -106,7 +106,7 @@ class AwsLambdaCustomRuntimeSpec extends BeanContextSpec  implements CommandOutp
         then:
         bootstrap.contains('#!/bin/sh')
         bootstrap.contains('set -euo pipefail')
-        bootstrap.contains('./foo -Xmx128m -Djava.library.path=$(pwd)')
+        bootstrap.contains('./foo -Xmx512m -Djava.library.path=$(pwd)')
 
         where:
         language << graalSupportedLanguages()
