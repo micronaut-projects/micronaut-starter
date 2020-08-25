705b63e16685071edd3d756dc47751b00c1bb663
Sergio del Amo <sergio.delamo@softamo.com>
2020-08-25 13:04:00 +0200

    feat: supported languages and default language for test framework (#413)

--- starter-core/src/test/groovy/io/micronaut/starter/options/TestFrameworkSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/options/TestFrameworkSpec.groovy
@@ -27,4 +27,32 @@ class TestFrameworkSpec extends Specification {
         path = '/{packagePath}/{className}'
     }
 
+    @Unroll("getDefaultLanguage for test framework: #testFramework return #expected")
+    void "verify the default language for a test framework"(Language expected, TestFramework testFramework) {
+        expect:
+        expected == testFramework.getDefaultLanguage()
+
+        where:
+        expected        | testFramework
+        Language.JAVA   | TestFramework.JUNIT
+        Language.GROOVY | TestFramework.SPOCK
+        Language.KOTLIN | TestFramework.KOTLINTEST
+    }
+
+    @Unroll("getSupportedLanguages for test framework: #testFramework return #expected")
+    void "verify the list of supported languages for a test framework"(List<Language> expected, TestFramework testFramework) {
+        given:
+        expect:
+        expected.sort { a, b -> a.name <=> b.name } ==
+                testFramework.supportedLanguages.sort { a, b -> a.name <=> b.name }
+
+        where:
+        expected                                          | testFramework
+        [Language.JAVA, Language.GROOVY, Language.KOTLIN] | TestFramework.JUNIT
+        [Language.GROOVY]                                 | TestFramework.SPOCK
+        [Language.KOTLIN]                                 | TestFramework.KOTLINTEST
+    }
+
+
+
 }
