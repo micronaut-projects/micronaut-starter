7325ce9786d62cbf2240ca9f1c7ecf0e27d187c1
Pavol Gressa <1838702+pgressa@users.noreply.github.com>
2020-09-10 09:02:54 +0200

    Add Knative service template support (#449)

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/knative/Knative.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.knative;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.feature.FeatureContext;
+import io.micronaut.starter.feature.knative.template.knativeYaml;
+import io.micronaut.starter.feature.jib.Jib;
+import io.micronaut.starter.feature.other.Management;
+import io.micronaut.starter.template.RockerTemplate;
+
+import javax.inject.Singleton;
+
+/**
+ * Adds Knative configuration to an application.
+ *
+ * @author Pavol Gressa
+ * @since 2.1
+ */
+@Singleton
+public class Knative implements Feature {
+
+    private final Jib jib;
+    private final Management management;
+
+    public Knative(Jib jib, Management management) {
+        this.jib = jib;
+        this.management = management;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "knative";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Knative Support";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Generates a knative deployment descriptor for deployment to Kubernetes";
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.CLOUD;
+    }
+
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        if (!featureContext.isPresent(Management.class)) {
+            featureContext.addFeature(management);
+        }
+        if (!featureContext.isPresent(Jib.class)) {
+            featureContext.addFeature(jib);
+        }
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addTemplate("knativeYaml", new RockerTemplate("knativeYaml.yml", knativeYaml.template(generatorContext.getProject())));
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return applicationType == ApplicationType.DEFAULT || applicationType == ApplicationType.GRPC;
+    }
+
+    @Nullable
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-kubernetes/latest/guide/index.html";
+    }
+
+    @Nullable
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://knative.dev/";
+    }
+}
---
+++ test-core/src/test/groovy/io/micronaut/starter/core/test/feature/knative/KnativeSpec.groovy
@@ -0,0 +1,46 @@
+package io.micronaut.starter.core.test.feature.knative
+
+import io.micronaut.starter.core.test.feature.testcontainers.book
+import io.micronaut.starter.core.test.feature.testcontainers.bookRepository
+import io.micronaut.starter.core.test.feature.testcontainers.bookRepositoryTest
+import io.micronaut.starter.feature.database.DatabaseDriverFeature
+import io.micronaut.starter.io.ConsoleOutput
+import io.micronaut.starter.io.FileSystemOutputHandler
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.template.RockerWritable
+import io.micronaut.starter.test.BuildToolCombinations
+import io.micronaut.starter.test.CommandSpec
+import spock.lang.Unroll
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+
+class KnativeSpec extends CommandSpec {
+
+    @Override
+    String getTempDirectoryPrefix() {
+        return "knative"
+    }
+
+    @Unroll
+    void "test knative template with #buildTool"(BuildTool buildTool) {
+        when:
+        generateProject(Language.JAVA, buildTool, ["knative"])
+
+        String output = null
+        if (buildTool == BuildTool.GRADLE) {
+            output = executeGradle("test")?.output
+        } else if (buildTool == BuildTool.MAVEN) {
+            output = executeMaven("compile test")
+        }
+
+        then:
+        output?.contains("BUILD SUCCESS")
+        Files.exists(Paths.get(dir.getPath(), "knativeYaml.yml"))
+
+        where:
+        buildTool << BuildToolCombinations.buildTools
+    }
+}
