721d428a851ab7a32c936eca554ff29a2cd0b06f
Sergio del Amo <sergio.delamo@softamo.com>
2020-09-30 12:32:39 +0200

    fix: applications with azure-function don't compile
    Close: https://github.com/micronaut-projects/micronaut-starter/issues/457

---
+++ test-core/src/test/groovy/io/micronaut/starter/core/test/azure/CreateAzureFunctionSpec.groovy
@@ -0,0 +1,36 @@
+package io.micronaut.starter.core.test.azure
+
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.options.TestFramework
+import io.micronaut.starter.test.ApplicationTypeCombinations
+import io.micronaut.starter.test.CommandSpec
+import spock.lang.Unroll
+
+class CreateAzureFunctionSpec extends CommandSpec {
+
+    @Override
+    String getTempDirectoryPrefix() {
+        "test-azure-function"
+    }
+
+    @Unroll
+    void 'create-#applicationType with features azure-function #lang and #build and test framework: #testFramework'(ApplicationType applicationType,
+                                                                                                                Language lang,
+                                                                                                                BuildTool build,
+                                                                                                                TestFramework testFramework) {
+        given:
+        List<String> features = ['azure-function']
+        generateProject(lang, build, features, applicationType, testFramework)
+
+        when:
+        String output = executeBuild(build, "test")
+
+        then:
+        output.contains("BUILD SUCCESS")
+
+        where:
+        [applicationType, lang, build, testFramework] << ApplicationTypeCombinations.combinations([ApplicationType.DEFAULT, ApplicationType.FUNCTION])
+    }
+}
