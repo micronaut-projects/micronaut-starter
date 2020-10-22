0d19199edb6a2a5e0f679f240a46d7785f249ed0
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2020-10-22 10:01:58 +0200

    Update Chocolatey files (#525)

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$version = '2.1.0'
+$version = '2.1.2'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   url           = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v$version/mn-win-amd64-v$version.zip"
-  checksum      = '81E9A7B0C194005EFA602354E04BBF607C2D99AA064A4FEAB002AD3540F73A8C'
+  checksum      = '1B37D3E61C03DC8C7CB1F61AF409FA958A97F6D6D4AE1C7CC7D9DEE032E283F1'
   checksumType  = 'sha256'
 }
 
