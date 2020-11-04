60ebc55a5d8363aca08903d1acd08dee5c7bf885
James Kleeh <james.kleeh@gmail.com>
2020-11-04 14:42:40 -0500

    Dont render {} for empty yaml config (#535)
    * Dont render {} for empty yaml config. Fixes #384
    
    * Add a comment instead of leaving the file empty

--- starter-core/src/main/java/io/micronaut/starter/template/YamlTemplate.java
+++ starter-core/src/main/java/io/micronaut/starter/template/YamlTemplate.java
@@ -47,8 +47,12 @@ public class YamlTemplate implements Template {
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
         options.setPrettyFlow(true);
 
-        Yaml yaml = new Yaml(options);
-        yaml.dump(config, new OutputStreamWriter(outputStream));
+        if (!config.isEmpty()) {
+            Yaml yaml = new Yaml(options);
+            yaml.dump(config, new OutputStreamWriter(outputStream));
+        } else {
+            outputStream.write("# Place application configuration here".getBytes());
+        }
     }
 
     @SuppressWarnings("unchecked")
