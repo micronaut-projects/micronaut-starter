b22aea272f6e6683d719ac9674b0b1c963ee5148
Dean Wette <wetted@ociweb.com>
2020-11-13 16:42:29 -0600

    Issue 229 - add feaure support for Project Lombok. Note that lombok must precede micronaut-inject-java for annotation processing.

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/other/ProjectLombok.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.other;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.LanguageSpecificFeature;
+import io.micronaut.starter.options.BuildTool;
+import io.micronaut.starter.options.Language;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class ProjectLombok implements LanguageSpecificFeature {
+
+    private static final String LOMBOK_MAVEN_KEY = "lombok.version";
+    private static final String LOMBOK_GRADLE_KEY = "lombokVersion";
+    private static final String LOMBOK_VERSION = "1.18.16";
+
+    @Override
+    public String getName() {
+        return "lombok";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Project Lombok";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for Project Lombok";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.DEV_TOOLS;
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://docs.micronaut.io/latest/guide/index.html#lombok";
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://projectlombok.org/features/all";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        String versionKey = generatorContext.getBuildTool().equals(BuildTool.MAVEN)
+            ? LOMBOK_MAVEN_KEY : LOMBOK_GRADLE_KEY;
+        generatorContext.getBuildProperties().put(versionKey, LOMBOK_VERSION);
+    }
+
+    @Override
+    public Language getRequiredLanguage() {
+        return Language.JAVA;
+    }
+}
