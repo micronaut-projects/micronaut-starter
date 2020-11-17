9044adce30529e14a38be2718eaa857a21ca8174
Graeme Rocher <graeme.rocher@oracle.com>
2020-11-17 08:43:04 +0100

    Fix broken gRPC service generation / add test (#559)

--- test-core/src/test/groovy/io/micronaut/starter/core/test/create/CreateGrpcSpec.groovy
+++ test-core/src/test/groovy/io/micronaut/starter/core/test/create/CreateGrpcSpec.groovy
@@ -1,6 +1,10 @@
 package io.micronaut.starter.core.test.create
 
 import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.cli.CodeGenConfig
+import io.micronaut.starter.cli.feature.grpc.CreateGrpcServiceCommand
+import io.micronaut.starter.cli.feature.grpc.CreateProtoServiceCommand
+import io.micronaut.starter.io.ConsoleOutput
 import io.micronaut.starter.test.CommandSpec
 import io.micronaut.starter.test.LanguageBuildCombinations
 import io.micronaut.starter.options.BuildTool
@@ -18,6 +22,41 @@ class CreateGrpcSpec extends CommandSpec {
         given:
         ApplicationType applicationType = ApplicationType.GRPC
         generateProject(lang, buildTool, [], applicationType)
+
+        CodeGenConfig codeGenConfig = CodeGenConfig.load(beanContext, dir, ConsoleOutput.NOOP)
+        ConsoleOutput consoleOutput = Mock(ConsoleOutput)
+
+        CreateProtoServiceCommand protoServiceCommand = new CreateProtoServiceCommand(
+                codeGenConfig,
+                getOutputHandler(consoleOutput),
+                consoleOutput
+
+        )
+        protoServiceCommand.setBeanContext(beanContext)
+
+        CreateGrpcServiceCommand command = new CreateGrpcServiceCommand(
+                codeGenConfig,
+                getOutputHandler(consoleOutput),
+                consoleOutput
+        ) {
+            @Override
+            protected CreateProtoServiceCommand getCreateProtoServiceCommand() {
+                return protoServiceCommand
+            }
+        }
+
+        command.setBeanContext(beanContext)
+        command.serviceName = "Greeting"
+
+        expect:
+        command.applies()
+
+        when:
+        command.call()
+
+        then:
+        1 * consoleOutput.out({ it.contains("Rendered Proto service") })
+        1 * consoleOutput.out({ it.contains("Rendered gRPC service") })
 
         when:
         String output = executeBuild(buildTool, "test")
