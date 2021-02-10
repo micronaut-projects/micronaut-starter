0e3856c9d08ce5472831d5c8d5ad4a7b779738d3
Sergio del Amo <sergio.delamo@softamo.com>
2021-02-10 23:00:26 +0100

    correct MicronautTest annotation
    udpate micronaut test annotation

--- starter-analytics-postgres/src/test/groovy/io/micronaut/starter/analytics/postgres/StoreGeneratedProjectStatsSpec.groovy
+++ starter-analytics-postgres/src/test/groovy/io/micronaut/starter/analytics/postgres/StoreGeneratedProjectStatsSpec.groovy
@@ -17,7 +17,7 @@ import io.micronaut.starter.options.JdkVersion
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.options.TestFramework
 import io.micronaut.starter.util.VersionInfo
-import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.micronaut.test.support.TestPropertyProvider
 import org.testcontainers.containers.PostgreSQLContainer
 import spock.lang.AutoCleanup
