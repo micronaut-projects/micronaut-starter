71e71ded50c594cc0790bbb4836ae7c5c543810d
Sergio del Amo <sergio.delamo@softamo.com>
2021-02-10 23:12:31 +0100

    revert changes to chocolatey

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$version = '2.3.1'
+$version = '2.1.3'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   url           = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v$version/mn-win-amd64-v$version.zip"
-  checksum      = '611D528B11E7289FDD458921EDB831B627A1CFB9884FA549AD0EF9BE67C94C72'
+  checksum      = '68ABCCEA80DF0B8BC3D9780736192610B198AE9BCC05753687AF69D0D3DF9617'
   checksumType  = 'sha256'
 }
 
