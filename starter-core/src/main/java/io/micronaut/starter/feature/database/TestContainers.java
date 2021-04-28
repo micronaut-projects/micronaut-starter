f5348b94053c0b05ebec7a184c997fadb84cc283
Graeme Rocher <graeme.rocher@oracle.com>
2021-04-28 20:32:51 +0200

    Remove unsupported oracle container image (#754)
    * Remove unsupported oracle container image
    
    * fix checkstyle

--- starter-core/src/main/java/io/micronaut/starter/feature/database/TestContainers.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/database/TestContainers.java
@@ -26,11 +26,9 @@ import io.micronaut.starter.feature.config.ApplicationConfiguration;
 import io.micronaut.starter.feature.config.Configuration;
 import io.micronaut.starter.feature.database.r2dbc.R2dbc;
 import io.micronaut.starter.options.TestFramework;
-import io.micronaut.starter.template.PropertiesTemplate;
 import io.micronaut.starter.template.StringTemplate;
 
 import javax.inject.Singleton;
-import java.util.Collections;
 
 @Singleton
 public class TestContainers implements Feature {
@@ -99,7 +97,6 @@ public class TestContainers implements Feature {
                     dependencyArtifactId = "mssqlserver";
                 } else if (driverFeature instanceof Oracle) {
                     url = "jdbc:tc:oracle:thin:@/xe";
-                    generatorContext.addTemplate("testContainersProperties", new PropertiesTemplate("src/test/resources/testcontainers.properties", Collections.singletonMap("oracle.container.image", "wnameless/oracle-xe-11g-r2")));
                     dependencyArtifactId = "oracle-xe";
                 }
 
