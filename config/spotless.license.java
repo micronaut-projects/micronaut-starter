87683869e04639055a6caf1f9a855025edebea4e
Graeme Rocher <graeme.rocher@oracle.com>
2021-05-02 14:03:26 +0200

    checkstyle

--- config/spotless.license.java
+++ config/spotless.license.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 original authors
+ * Copyright 2017-$YEAR original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.starter.feature;
-
-import java.util.Optional;
-import java.util.function.Predicate;
-
-public interface FeaturePredicate extends Predicate<Feature> {
-
-    default Optional<String> getWarning() {
-        return Optional.empty();
-    }
-}
