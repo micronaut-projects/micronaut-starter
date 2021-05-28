c9e9661ffb1b0b7e8e86a33e96ceb569d9dbbcd2
Iván López <lopezi@objectcomputing.com>
2021-05-28 15:14:59 +0200

    Fix CLI native image generation (#818)
    * Fix cli native image generation
    
    * Use GraalVM JDK11

--- install-graal.sh
+++ install-graal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-GRAAL_FILENAME="graalvm-ce-java8-${GRAAL_OS}-${GRAAL_VERSION}.tar.gz"
+GRAAL_FILENAME="graalvm-ce-java11-${GRAAL_OS}-${GRAAL_VERSION}.tar.gz"
 
 mkdir tmp
 curl -4 -L https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAAL_VERSION}/${GRAAL_FILENAME} -o tmp/${GRAAL_FILENAME}
-tar -zxvf tmp/${GRAAL_FILENAME} -C tmp && mv tmp/graalvm-ce-java8-${GRAAL_VERSION} graalvm
+tar -zxvf tmp/${GRAAL_FILENAME} -C tmp && mv tmp/graalvm-ce-java11-${GRAAL_VERSION} graalvm
 rm -rf tmp/*
