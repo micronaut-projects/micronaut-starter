d3f5338e221d1cfdf59a986dbc17ed0b7763a9a0
Sergio del Amo <sergio.delamo@softamo.com>
2021-08-16 16:35:44 +0200

    refactor: rename RxJava features (#878)

--- starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJava1.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJava1.java
@@ -22,7 +22,7 @@ import javax.inject.Singleton;
 import io.micronaut.starter.feature.reactive.ReactiveFeature;
 
 @Singleton
-public class RxJavaOne implements ReactiveFeature {
+public class RxJava1 implements ReactiveFeature {
     @NonNull
     @Override
     public String getName() {
--- starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJava3.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJava3.java
@@ -22,7 +22,7 @@ import javax.inject.Singleton;
 import io.micronaut.starter.feature.reactive.ReactiveFeature;
 
 @Singleton
-public class RxJavaThree implements ReactiveFeature {
+public class RxJava3 implements ReactiveFeature {
 
     @NonNull
     @Override
