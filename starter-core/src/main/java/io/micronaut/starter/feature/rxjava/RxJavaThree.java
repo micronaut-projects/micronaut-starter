acf84650903902f324e4d5317874b7c3335fde53
Sergio del Amo <sergio.delamo@softamo.com>
2021-08-16 12:36:01 +0200

    feat: throw exception if multiple reactive libraries (#877)

--- starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJavaThree.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/rxjava/RxJavaThree.java
@@ -16,20 +16,13 @@
 package io.micronaut.starter.feature.rxjava;
 
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.starter.application.ApplicationType;
 import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
-import io.micronaut.starter.feature.Category;
-import io.micronaut.starter.feature.Feature;
-
 import javax.inject.Singleton;
+import io.micronaut.starter.feature.reactive.ReactiveFeature;
 
 @Singleton
-public class RxJavaThree implements Feature {
-    @Override
-    public boolean supports(ApplicationType applicationType) {
-        return true;
-    }
+public class RxJavaThree implements ReactiveFeature {
 
     @NonNull
     @Override
@@ -45,11 +38,6 @@ public class RxJavaThree implements Feature {
     @Override
     public String getDescription() {
         return "Adds support for RxJava 3 to a Micronaut application; Converters and Instrumentation for RxJava 3 types, RxJava 3 compatible HTTP Client";
-    }
-
-    @Override
-    public String getCategory() {
-        return Category.REACTIVE;
     }
 
     @Override
