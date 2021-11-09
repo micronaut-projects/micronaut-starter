58823c11c51c7916b0e34271281eeaefbefc5f1c
Pavol Gressa <pavol.gressa@oracle.com>
2021-11-09 14:25:43 +0100

    feat: add kubernetes-informer feature

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/k8s/KubernetesInformer.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.k8s;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class KubernetesInformer implements Feature {
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "kubernetes-informer";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Kubernetes Informer Support";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds Micronaut Kubernetes Informer support";
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.CLOUD;
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-kubernetes/latest/guide/#kubernetes-informer";
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://github.com/kubernetes-client/java/wiki";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addDependency(Dependency.builder()
+                .groupId(KubernetesClient.MICRONAUT_KUBERNETES_GROUP_ID)
+                .artifactId("micronaut-kubernetes-informer")
+                .compile());
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/k8s/KubernetesInformerSpec.groovy
@@ -0,0 +1,86 @@
+package io.micronaut.starter.feature.k8s
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import spock.lang.Shared
+import spock.lang.Subject
+import spock.lang.Unroll
+
+class KubernetesInformerSpec extends ApplicationContextSpec  implements CommandOutputFixture {
+
+    void 'test readme.md with feature kubernetes-informer contains links to docs'() {
+        when:
+        def output = generate(['kubernetes-informer'])
+        def readme = output["README.md"]
+
+        then:
+        readme
+        readme.contains("https://github.com/kubernetes-client/java/wiki")
+        readme.contains("https://micronaut-projects.github.io/micronaut-kubernetes/latest/guide/#kubernetes-informer")
+    }
+
+    @Subject
+    @Shared
+    KubernetesInformer informer = beanContext.getBean(KubernetesInformer)
+
+    void "kubernetes-informer belongs to Client category"() {
+        expect:
+        Category.CLOUD == informer.category
+    }
+
+    void "kubernetes-informer title and description are different"() {
+        expect:
+        informer.getTitle()
+        informer.getDescription()
+        informer.getTitle() != informer.getDescription()
+    }
+
+    @Unroll("feature kubernetes-informer works for application type: #applicationType")
+    void "feature kubernetes-informer works for every type of application type"(ApplicationType applicationType) {
+        expect:
+        informer.supports(applicationType)
+
+        where:
+        applicationType << ApplicationType.values()
+    }
+
+    @Unroll
+    void 'dependency is included with maven and feature kubernetes-informer for language=#language'(Language language) {
+        when:
+        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+                .features(['kubernetes-informer'])
+                .language(language)
+                .render()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.kubernetes</groupId>
+      <artifactId>micronaut-kubernetes-informer</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+        where:
+        language << Language.values().toList()
+    }
+
+    @Unroll
+    void 'dependency is included with gradle and kubernetes-informer reactor for language=#language'(Language language) {
+        when:
+        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
+                .language(language)
+                .features(['kubernetes-informer'])
+                .render()
+
+        then:
+        template.contains('implementation("io.micronaut.kubernetes:micronaut-kubernetes-informer")')
+
+        where:
+        language << Language.values()
+    }
+}
