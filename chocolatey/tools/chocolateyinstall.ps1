12941eff801d0dac51ad0880290f991537b96d8e
micronaut-build <micronaut-build-account@grails.org>
2021-11-12 21:45:01 +0000

    Bump Micronaut Chocolatey package to v3.1.4

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$version = '3.1.3'
+$version = '3.1.4'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   url           = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v$version/mn-win-amd64-v$version.zip"
-  checksum      = 'DC4941316710F27DE93D2627C80668782C4A3E90E569C59030149FE37422DB81'
+  checksum      = 'D43F51C8068CF318EB332A7871AFAA49217DD137A2FA8F9AD349E57680608EA0'
   checksumType  = 'sha256'
 }
 
