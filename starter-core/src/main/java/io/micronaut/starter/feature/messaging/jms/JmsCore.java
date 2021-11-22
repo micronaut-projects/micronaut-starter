bc96deea88a0a24616a5c9e8e5424de796d47f80
Pavol Gressa <pavol.gressa@oracle.com>
2021-11-22 19:02:54 +0100

    feat: add oracle advanced queuing + jms-core

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/messaging/jms/JmsCore.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.messaging.jms;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class JmsCore extends AbstractJmsFeature {
+
+    public static final String NAME = "jms-core";
+
+    @NonNull
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Micronaut JMS";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds Micronaut support for JMS";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addDependency(Dependency.builder()
+                .groupId("io.micronaut.jms")
+                .artifactId("micronaut-jms-core")
+                .compile());
+    }
+}
