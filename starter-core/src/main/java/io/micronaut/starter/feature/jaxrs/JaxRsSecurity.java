2ac2c068c13043fa9b51ba1490b22aa870450de2
Pavol Gressa <pavol.gressa@oracle.com>
2021-11-22 15:18:50 +0100

    files

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/jaxrs/JaxRsSecurity.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.jaxrs;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.feature.server.MicronautServerDependent;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class JaxRsSecurity implements Feature, MicronautServerDependent {
+
+    @Override
+    public String getName() {
+        return "jax-rs-security";
+    }
+
+    @Override
+    public String getTitle() {
+        return "JAX-RS Micronaut Security support";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Integrates Micronaut Security with JAX-RS";
+    }
+
+    @Override
+    public boolean isVisible() {
+        return false;
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addDependency(Dependency.builder()
+                .groupId(JaxRs.MICRONAUT_JAX_RS_GROUP)
+                .artifactId("micronaut-jaxrs-server-security")
+                .versionProperty("micronaut.jaxrs.version")
+                .compile());
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return applicationType == ApplicationType.DEFAULT;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.SERVER;
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-jaxrs/latest/guide/index.html";
+    }
+}
