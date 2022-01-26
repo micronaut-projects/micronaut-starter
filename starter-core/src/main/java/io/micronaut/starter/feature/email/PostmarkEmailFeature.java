bde4ef9531ab08b44eb27efc993f2ef0285b4b30
Sergio del Amo <sergio.delamo@softamo.com>
2022-01-26 10:30:07 +0100

    add email feature

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/email/PostmarkEmailFeature.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.email;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class PostmarkEmailFeature implements EmailProviderFeature {
+
+    private final TemplateEmailFeature templateEmailFeature;
+
+    public PostmarkEmailFeature(TemplateEmailFeature templateEmailFeature) {
+        this.templateEmailFeature = templateEmailFeature;
+    }
+
+    @Override
+    @NonNull
+    public TemplateEmailFeature getTemplateFeature() {
+        return this.templateEmailFeature;
+    }
+
+    @Override
+    @NonNull
+    public String getDescription() {
+        return "Integration with Postmark to send transactional emails";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Postmark Email";
+    }
+
+    @Override
+    public String getModule() {
+        return "postmark";
+    }
+
+    @Override
+    @Nullable
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-email/latest/guide/index.html#postmark";
+    }
+
+    @Override
+    @Nullable
+    public String getThirdPartyDocumentation() {
+        return "https://postmarkapp.com";
+    }
+}
