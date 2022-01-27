4e42dfd528c2bdd63f02a84e5f136d7ac4e30523
Burt Beckwith <burt@burtbeckwith.com>
2022-01-27 14:39:04 -0500

    update features for micronaut-tracing (#1050)

--- starter-core/src/test/groovy/io/micronaut/starter/feature/tracing/JaegerSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/tracing/JaegerSpec.groovy
@@ -4,34 +4,35 @@ import io.micronaut.starter.ApplicationContextSpec
 import io.micronaut.starter.BuildBuilder
 import io.micronaut.starter.application.generator.GeneratorContext
 import io.micronaut.starter.fixture.CommandOutputFixture
-import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import spock.lang.Unroll
+
+import static io.micronaut.starter.options.BuildTool.GRADLE
+import static io.micronaut.starter.options.BuildTool.MAVEN
 
 class JaegerSpec extends ApplicationContextSpec implements CommandOutputFixture {
 
     void 'test readme.md with feature tracing-jaeger contains links to micronaut docs'() {
         when:
-        def output = generate(['tracing-jaeger'])
-        def readme = output["README.md"]
+        Map output = generate(['tracing-jaeger'])
+        String readme = output['README.md']
 
         then:
         readme
-        readme.contains("https://docs.micronaut.io/latest/guide/index.html#jaeger")
-        readme.contains("https://www.jaegertracing.io")
+        readme.contains 'https://micronaut-projects.github.io/micronaut-tracing/latest/guide/#jaeger'
+        readme.contains 'https://www.jaegertracing.io/'
     }
 
     @Unroll
     void 'test gradle tracing-jaeger feature for language=#language'() {
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
+        String template = new BuildBuilder(beanContext, GRADLE)
                 .language(language)
                 .features(['tracing-jaeger'])
                 .render()
 
         then:
-        template.contains('implementation("io.micronaut:micronaut-tracing")')
-        template.contains('runtimeOnly("io.jaegertracing:jaeger-thrift")')
+        template.contains 'implementation("io.micronaut.tracing:tracing-jaeger")'
 
         where:
         language << Language.values().toList()
@@ -40,26 +41,19 @@ class JaegerSpec extends ApplicationContextSpec implements CommandOutputFixture 
     @Unroll
     void 'test maven tracing-jaeger feature for language=#language'() {
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+        String template = new BuildBuilder(beanContext, MAVEN)
                 .language(language)
                 .features(['tracing-jaeger'])
                 .render()
 
         then:
-        template.contains("""
+        template.contains '''
     <dependency>
-      <groupId>io.micronaut</groupId>
-      <artifactId>micronaut-tracing</artifactId>
+      <groupId>io.micronaut.tracing</groupId>
+      <artifactId>tracing-jaeger</artifactId>
       <scope>compile</scope>
     </dependency>
-""")
-        template.contains("""
-    <dependency>
-      <groupId>io.jaegertracing</groupId>
-      <artifactId>jaeger-thrift</artifactId>
-      <scope>runtime</scope>
-    </dependency>
-""")
+'''
 
         where:
         language << Language.values().toList()
@@ -70,8 +64,7 @@ class JaegerSpec extends ApplicationContextSpec implements CommandOutputFixture 
         GeneratorContext commandContext = buildGeneratorContext(['tracing-jaeger'])
 
         then:
-        commandContext.configuration.get('tracing.jaeger.enabled'.toString()) == true
-        commandContext.configuration.get('tracing.jaeger.sampler.probability'.toString()) == 0.1
+        commandContext.configuration.get('tracing.jaeger.enabled') == true
+        commandContext.configuration.get('tracing.jaeger.sampler.probability') == 0.1
     }
-
 }
--- starter-core/src/test/groovy/io/micronaut/starter/feature/tracing/ZipkinSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/tracing/ZipkinSpec.groovy
@@ -4,36 +4,35 @@ import io.micronaut.starter.ApplicationContextSpec
 import io.micronaut.starter.BuildBuilder
 import io.micronaut.starter.application.generator.GeneratorContext
 import io.micronaut.starter.fixture.CommandOutputFixture
-import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import spock.lang.Unroll
 
-class ZipkinSpec extends ApplicationContextSpec  implements CommandOutputFixture {
+import static io.micronaut.starter.options.BuildTool.GRADLE
+import static io.micronaut.starter.options.BuildTool.MAVEN
+
+class ZipkinSpec extends ApplicationContextSpec implements CommandOutputFixture {
 
     void 'test readme.md with feature tracing-zipkin contains links to micronaut docs'() {
         when:
-        def output = generate(['tracing-zipkin'])
-        def readme = output["README.md"]
+        Map output = generate(['tracing-zipkin'])
+        String readme = output['README.md']
 
         then:
         readme
-        readme.contains("https://docs.micronaut.io/latest/guide/index.html#zipkin")
-        readme.contains("https://docs.micronaut.io/latest/guide/index.html#zipkin")
+        readme.contains 'https://micronaut-projects.github.io/micronaut-tracing/latest/guide/#zipkin'
+        readme.contains 'https://zipkin.io/'
     }
 
     @Unroll
     void 'test gradle tracing-zipkin feature for language=#language'() {
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
+        String template = new BuildBuilder(beanContext, GRADLE)
                 .language(language)
                 .features(['tracing-zipkin'])
                 .render()
 
         then:
-        template.contains('implementation("io.micronaut:micronaut-tracing")')
-        template.contains('implementation("io.opentracing.brave:brave-opentracing")')
-        template.contains('runtimeOnly("io.zipkin.brave:brave-instrumentation-http")')
-        template.contains('runtimeOnly("io.zipkin.reporter2:zipkin-reporter")')
+        template.contains 'implementation("io.micronaut.tracing:tracing-zipkin")'
 
         where:
         language << Language.values().toList()
@@ -42,40 +41,19 @@ class ZipkinSpec extends ApplicationContextSpec  implements CommandOutputFixture
     @Unroll
     void 'test maven tracing-zipkin feature for language=#language'() {
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+        String template = new BuildBuilder(beanContext, MAVEN)
                 .language(language)
                 .features(['tracing-zipkin'])
                 .render()
 
         then:
-        template.contains("""
+        template.contains '''
     <dependency>
-      <groupId>io.micronaut</groupId>
-      <artifactId>micronaut-tracing</artifactId>
+      <groupId>io.micronaut.tracing</groupId>
+      <artifactId>tracing-zipkin</artifactId>
       <scope>compile</scope>
     </dependency>
-""")
-        template.contains("""
-    <dependency>
-      <groupId>io.opentracing.brave</groupId>
-      <artifactId>brave-opentracing</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
-        template.contains("""
-    <dependency>
-      <groupId>io.zipkin.brave</groupId>
-      <artifactId>brave-instrumentation-http</artifactId>
-      <scope>runtime</scope>
-    </dependency>
-""")
-        template.contains("""
-    <dependency>
-      <groupId>io.zipkin.reporter2</groupId>
-      <artifactId>zipkin-reporter</artifactId>
-      <scope>runtime</scope>
-    </dependency>
-""")
+'''
 
         where:
         language << Language.values().toList()
@@ -86,9 +64,8 @@ class ZipkinSpec extends ApplicationContextSpec  implements CommandOutputFixture
         GeneratorContext commandContext = buildGeneratorContext(['tracing-zipkin'])
 
         then:
-        commandContext.configuration.get('tracing.zipkin.enabled'.toString()) == true
-        commandContext.configuration.get('tracing.zipkin.http.url'.toString()) == 'http://localhost:9411'
-        commandContext.configuration.get('tracing.zipkin.sampler.probability'.toString()) == 0.1
+        commandContext.configuration.get('tracing.zipkin.enabled') == true
+        commandContext.configuration.get('tracing.zipkin.http.url') == 'http://localhost:9411'
+        commandContext.configuration.get('tracing.zipkin.sampler.probability') == 0.1
     }
-
 }
