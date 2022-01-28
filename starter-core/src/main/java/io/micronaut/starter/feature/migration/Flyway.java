b3261b0c7bdca200ee425b4d01247ef3f9ae1599
jameskleeh <james.kleeh@gmail.com>
2022-01-28 11:30:55 -0500

    Also check for mariadb

--- starter-core/src/main/java/io/micronaut/starter/feature/migration/Flyway.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/migration/Flyway.java
@@ -17,6 +17,7 @@ package io.micronaut.starter.feature.migration;
 
 import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.feature.database.MariaDB;
 import io.micronaut.starter.feature.database.MySQL;
 import io.micronaut.starter.feature.database.SQLServer;
 import jakarta.inject.Singleton;
@@ -55,7 +56,7 @@ public class Flyway implements MigrationFeature {
                 .groupId("io.micronaut.flyway")
                 .artifactId("micronaut-flyway")
                 .compile());
-        if (generatorContext.isFeaturePresent(MySQL.class)) {
+        if (generatorContext.isFeaturePresent(MySQL.class) || generatorContext.isFeaturePresent(MariaDB.class)) {
             generatorContext.addDependency(Dependency.builder()
                     .groupId("org.flywaydb")
                     .artifactId("flyway-mysql")
