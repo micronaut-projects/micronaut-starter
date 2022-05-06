50aec8f93fe6c72e6d610b1079d862a4e38bee8c
Tim Yates <tim.yates@gmail.com>
2022-05-06 16:21:23 +0100

    Swtich to using Order instead of janky Comparators

--- starter-core/src/main/java/io/micronaut/starter/build/maven/MavenBuildCreator.java
+++ starter-core/src/main/java/io/micronaut/starter/build/maven/MavenBuildCreator.java
@@ -23,54 +23,17 @@ import io.micronaut.starter.build.dependencies.Coordinate;
 import io.micronaut.starter.build.dependencies.Dependency;
 import io.micronaut.starter.build.dependencies.DependencyCoordinate;
 import io.micronaut.starter.build.dependencies.Phase;
+import io.micronaut.starter.build.dependencies.Priority;
 import io.micronaut.starter.build.dependencies.Source;
 import io.micronaut.starter.options.Language;
 import jakarta.inject.Singleton;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Singleton
 public class MavenBuildCreator {
-
-    private static final String MICRONAUT_INJECT_JAVA = "micronaut-inject-java";
-    private static final String MICRONAUT_INJECT = "micronaut-inject";
-    private static final String MICRONAUT_DATA_PROCESSOR = "micronaut-data-processor";
-
-    /**
-     * For Maven annotation processing, the dependencies must be in a specific order, as in Maven, the first wins no matter what.
-     *
-     * This comparator ensures that the order is as follows (if they exist):
-     *
-     * - lombok
-     * - micronaut-inject
-     * - micronaut-inject-java
-     * - micronaut-data-processor
-     * - other dependencies
-     */
-    private static final Comparator<? super Coordinate> PROCESSOR_COMPARATOR = (o1, o2) -> {
-        if ("lombok".equals(o1.getArtifactId()) && "org.projectlombok".equals(o1.getGroupId())) {
-            return Integer.MIN_VALUE;
-        } else if ("lombok".equals(o2.getArtifactId()) && "org.projectlombok".equals(o2.getGroupId())) {
-            return Integer.MAX_VALUE;
-        } else if (MICRONAUT_INJECT.equals(o1.getArtifactId())) {
-            return Integer.MIN_VALUE + 1;
-        } else if (MICRONAUT_INJECT.equals(o2.getArtifactId())) {
-            return Integer.MAX_VALUE - 1;
-        } else if (MICRONAUT_INJECT_JAVA.equals(o1.getArtifactId())) {
-            return Integer.MIN_VALUE + 2;
-        } else if (MICRONAUT_INJECT_JAVA.equals(o2.getArtifactId())) {
-            return Integer.MAX_VALUE - 2;
-        } else if (MICRONAUT_DATA_PROCESSOR.equals(o1.getArtifactId())) {
-            return Integer.MIN_VALUE + 3;
-        } else if (MICRONAUT_DATA_PROCESSOR.equals(o2.getArtifactId())) {
-            return Integer.MAX_VALUE - 3;
-        } else {
-            return Coordinate.COMPARATOR.compare(o1, o2);
-        }
-    };
 
     @NonNull
     public MavenBuild create(GeneratorContext generatorContext) {
@@ -101,8 +64,9 @@ public class MavenBuildCreator {
 
         Coordinate injectJava = Dependency.builder()
                 .groupId("io.micronaut")
-                .artifactId(MICRONAUT_INJECT_JAVA)
+                .artifactId("micronaut-inject-java")
                 .versionProperty("micronaut.version")
+                .order(Priority.MICRONAUT_INJECT_JAVA.getOrder())
                 .buildCoordinate(true);
         Coordinate validation = Dependency.builder()
                 .groupId("io.micronaut")
@@ -126,8 +90,8 @@ public class MavenBuildCreator {
             annotationProcessorsCoordinates.add(mnGraal);
         }
 
-        annotationProcessorsCoordinates.sort(PROCESSOR_COMPARATOR);
-        testAnnotationProcessorsCoordinates.sort(PROCESSOR_COMPARATOR);
+        annotationProcessorsCoordinates.sort(Coordinate.COMPARATOR);
+        testAnnotationProcessorsCoordinates.sort(Coordinate.COMPARATOR);
 
         List<MavenPlugin> plugins = generatorContext.getBuildPlugins()
                 .stream()
