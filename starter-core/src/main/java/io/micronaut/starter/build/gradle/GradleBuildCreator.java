a8b1e7103164645d03efad14e00154cedb750ecf
Sergio del Amo <sergio.delamo@softamo.com>
2022-05-09 22:07:55 +0200

    feat: programmatically define repositories (#1210)
    * programmatically define repositories
    
    * remove ? extends GradleRepository

--- starter-core/src/main/java/io/micronaut/starter/build/gradle/GradleBuildCreator.java
+++ starter-core/src/main/java/io/micronaut/starter/build/gradle/GradleBuildCreator.java
@@ -19,7 +19,9 @@ import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.order.OrderUtil;
 import io.micronaut.starter.application.generator.GeneratorContext;
 
+import io.micronaut.starter.build.Repository;
 import jakarta.inject.Singleton;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +29,7 @@ import java.util.stream.Collectors;
 public class GradleBuildCreator {
 
     @NonNull
-    public GradleBuild create(@NonNull GeneratorContext generatorContext) {
+    public GradleBuild create(@NonNull GeneratorContext generatorContext, List<Repository> repositories) {
         GradleDsl gradleDsl = generatorContext
                 .getBuildTool()
                 .getGradleDsl()
@@ -38,15 +40,9 @@ public class GradleBuildCreator {
                 .map(GradlePlugin.class::cast)
                 .sorted(OrderUtil.COMPARATOR)
                 .collect(Collectors.toList());
-        return new GradleBuild(gradleDsl, resolveDependencies(generatorContext), gradlePlugins);
+        return new GradleBuild(gradleDsl, GradleDependency.listOf(generatorContext), gradlePlugins,
+                GradleRepository.listOf(generatorContext.getBuildTool().getGradleDsl().orElse(GradleDsl.GROOVY),
+                        repositories));
     }
 
-    @NonNull
-    private List<GradleDependency> resolveDependencies(@NonNull GeneratorContext generatorContext) {
-        return generatorContext.getDependencies()
-                .stream()
-                .map(dep -> new GradleDependency(dep, generatorContext))
-                .sorted(GradleDependency.COMPARATOR)
-                .collect(Collectors.toList());
-    }
 }
---
+++ starter-core/src/main/java/io/micronaut/starter/build/gradle/GradleRepository.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.build.gradle;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.starter.build.MavenCentral;
+import io.micronaut.starter.build.Repository;
+import io.micronaut.starter.template.Writable;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GradleRepository implements Writable  {
+    private final GradleDsl gradleDsl;
+    private final String url;
+
+    public GradleRepository(GradleDsl gradleDsl, String url) {
+        this.gradleDsl = gradleDsl;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public GradleDsl getGradleDsl() {
+        return gradleDsl;
+    }
+
+    @Override
+    public void write(OutputStream outputStream) throws IOException {
+        String content = getGradleDsl() == GradleDsl.KOTLIN ?
+                "maven(\"" + getUrl() + "\")" :
+                "maven { url \"" + getUrl() + "\" }";
+        outputStream.write(content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @NonNull
+    public static List<GradleRepository> listOf(GradleDsl gradleDsl, List<Repository> repositories) {
+        return repositories.stream()
+                .map(it -> {
+                    if (it instanceof MavenCentral) {
+                        return new GradleMavenCentral(gradleDsl, it.getUrl());
+                    }
+                    return new GradleRepository(gradleDsl, it.getUrl());
+                }).collect(Collectors.toList());
+    }
+}
