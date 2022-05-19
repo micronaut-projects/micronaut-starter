e3a7ae551e2048c8795c3c694d5c7d8a64bb70e4
Sergio del Amo <sergio.delamo@softamo.com>
2022-05-19 18:37:55 +0200

    add graalvm tests for function-url

--- test-lambda.sh
+++ test-lambda.sh
@@ -50,6 +50,52 @@ cd ../..
 rm -rf starter-cli/temp
 
 ############
+# | BUILD  | TYPE     | RUNTIME  | FEATURES
+# | GRADLE | FUNCTION | PROVIDED | aws-lambda,aws-cdk,aws-lambda-function-url,graalvm
+############
+
+./gradlew micronaut-cli:run --args="create-function-app -b gradle -f \"aws-lambda,aws-cdk,aws-lambda-function-url,graalvm\" temp" || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then  
+  exit $EXIT_STATUS
+fi
+
+cd starter-cli/temp
+
+./test-lambda.sh  || EXIT_STATUS=$? 
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "❌ FAILED | GRADLE | FUNCTION | PROVIDED    | aws-lambda,aws-cdk,aws-lambda-function-url,graalvm"
+  exit $EXIT_STATUS
+fi
+
+cd ../..
+rm -rf starter-cli/temp
+
+############
+# | BUILD  | TYPE     | RUNTIME | FEATURES
+# | MAVEN | FUNCTION  | PROVIDED    | aws-lambda,aws-cdk,aws-lambda-function-url,graalvm
+############
+
+# ./gradlew micronaut-cli:run --args="create-function-app -b maven -f \"aws-lambda,aws-cdk,aws-lambda-function-url,graalvm\" temp" || EXIT_STATUS=$?
+# 
+# if [ $EXIT_STATUS -ne 0 ]; then
+#   exit $EXIT_STATUS
+# fi
+# 
+# cd starter-cli/temp
+# 
+# ./test-lambda.sh  || EXIT_STATUS=$? 
+# 
+# if [ $EXIT_STATUS -ne 0 ]; then
+#   echo "❌ FAILED | MAVEN | FUNCTION  | PROVIDED    | aws-lambda,aws-cdk,aws-lambda-function-url,graalvm"
+#   exit $EXIT_STATUS
+# fi
+# 
+# cd ../..
+# rm -rf starter-cli/temp
+
+############
 # | BUILD  | TYPE | RUNTIME | FEATURES
 # | MAVEN  | APP  | JAVA    | aws-lambda,aws-cdk,amazon-api-gateway
 ############
