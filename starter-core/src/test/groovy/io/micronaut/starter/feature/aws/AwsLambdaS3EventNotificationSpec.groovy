03efc1c8dc44be24e0f884d5e6889a4829f26710
Sergio del Amo <sergio.delamo@softamo.com>
2022-05-22 11:35:24 +0200

    add unit tests

---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/aws/AwsLambdaS3EventNotificationSpec.groovy
@@ -0,0 +1,35 @@
+package io.micronaut.starter.feature.aws
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.feature.Category
+import spock.lang.Subject
+
+class AwsLambdaS3EventNotificationSpec  extends ApplicationContextSpec {
+
+    @Subject
+    AwsLambdaS3EventNotification awsLambdaS3EventNotification = beanContext.getBean(AwsLambdaS3EventNotification)
+
+    void 'aws-lambda-s3-event-notification feature is in the cloud category'() {
+        expect:
+        awsLambdaS3EventNotification.category == Category.CLOUD
+    }
+
+    void 'aws-lambda-s3-event-notification feature is an instance of AwsLambdaEventFeature'() {
+        expect:
+        awsLambdaS3EventNotification instanceof AwsLambdaEventFeature
+    }
+
+    void "aws-lambda-s3-event-notification does not support #applicationType application type"(ApplicationType applicationType) {
+        expect:
+        !awsLambdaS3EventNotification.supports(applicationType)
+
+        where:
+        applicationType << (ApplicationType.values() - ApplicationType.FUNCTION)
+    }
+
+    void "aws-lambda-s3-event-notification supports function application type"() {
+        expect:
+        awsLambdaS3EventNotification.supports(ApplicationType.FUNCTION)
+    }
+}
