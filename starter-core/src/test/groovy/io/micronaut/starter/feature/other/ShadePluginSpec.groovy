4e38eefa32b540234a7c42bc6bf7302c0252ef06
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-02 14:15:11 +0200

    remove unused check

--- starter-core/src/test/groovy/io/micronaut/starter/feature/other/ShadePluginSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/other/ShadePluginSpec.groovy
@@ -41,12 +41,7 @@ class ShadePluginSpec extends ApplicationContextSpec  implements CommandOutputFi
         then:
         noExceptionThrown()
         semanticVersionOptional.isPresent()
-
-        if (applicationType == ApplicationType.DEFAULT) {
-            assert !template.contains(dependency)
-        } else if (applicationType == ApplicationType.FUNCTION) {
-            assert template.contains(dependency)
-        }
+        
         where:
         [language, applicationType] << [Language.values(), [ApplicationType.FUNCTION, ApplicationType.DEFAULT]].combinations()
     }
