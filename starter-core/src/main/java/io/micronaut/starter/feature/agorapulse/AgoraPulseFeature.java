74bbd85cc4e56b1ec655e248a58925c8de94664e
Vladimir Orany <vladimir@orany.cz>
2022-06-21 08:34:28 +0200

    Added Micronaut Permissions feature (#1250)

--- starter-core/src/main/java/io/micronaut/starter/feature/agorapulse/AgoraPulseFeature.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/agorapulse/AgoraPulseFeature.java
@@ -15,8 +15,16 @@
  */
 package io.micronaut.starter.feature.agorapulse;
 
+import com.fizzed.rocker.RockerModel;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.feature.CommunityFeature;
+import io.micronaut.starter.options.Language;
+import io.micronaut.starter.options.TestFramework;
+import io.micronaut.starter.template.RockerTemplate;
+
+import java.util.Optional;
 
 public interface AgoraPulseFeature extends CommunityFeature {
 
@@ -33,4 +41,68 @@ public interface AgoraPulseFeature extends CommunityFeature {
     default String getCommunityContributor() {
         return "Agorapulse";
     }
+
+    static Optional<RockerModel> mainModel(
+            GeneratorContext generatorContext,
+            @Nullable RockerModel javaModel,
+            @Nullable RockerModel groovyModel,
+            @Nullable RockerModel kotlinModel
+    ) {
+        switch (generatorContext.getLanguage()) {
+            case JAVA:
+                return Optional.ofNullable(javaModel);
+            case GROOVY:
+                return Optional.ofNullable(groovyModel);
+            case KOTLIN:
+                return Optional.ofNullable(kotlinModel);
+            default:
+                return Optional.empty();
+        }
+    }
+
+    @NonNull
+    static Optional<RockerModel> testModel(
+            GeneratorContext generatorContext,
+            @Nullable RockerModel javaModel,
+            @Nullable RockerModel groovyModel,
+            @Nullable RockerModel kotlinJUnitModel,
+            @Nullable RockerModel kotestModel
+    ) {
+        if (generatorContext.getLanguage() == Language.JAVA && generatorContext.getTestFramework() == TestFramework.JUNIT) {
+            return Optional.ofNullable(javaModel);
+        }
+
+        if (generatorContext.getLanguage() == Language.GROOVY && generatorContext.getTestFramework() == TestFramework.SPOCK) {
+            return Optional.ofNullable(groovyModel);
+        }
+
+        if (generatorContext.getLanguage() == Language.KOTLIN && generatorContext.getTestFramework() == TestFramework.JUNIT) {
+            return Optional.ofNullable(kotlinJUnitModel);
+        }
+
+        if (generatorContext.getLanguage() == Language.KOTLIN && generatorContext.getTestFramework() == TestFramework.KOTEST) {
+            return Optional.ofNullable(kotestModel);
+        }
+        return Optional.empty();
+    }
+
+    static void addMain(GeneratorContext generatorContext, String className, RockerModel rockerModel, String templateName) {
+        String extension = generatorContext.getLanguage().getExtension();
+        String srcDir = generatorContext.getLanguage().getSrcDir();
+        generatorContext.addTemplate(
+                templateName,
+                new RockerTemplate(srcDir + "/{packagePath}/" + className + "." + extension, rockerModel)
+        );
+    }
+
+    static void addTest(GeneratorContext generatorContext, String className, RockerModel rockerModel, String templateName) {
+        String testSrcDir = generatorContext.getLanguage().getTestSrcDir();
+        String extension = generatorContext.getLanguage().getExtension();
+        String testFrameworkSuffix = generatorContext.getTestFramework().getTestFrameworkSuffix();
+        generatorContext.addTemplate(
+                templateName,
+                new RockerTemplate(testSrcDir + "/{packagePath}/" + className + testFrameworkSuffix + extension, rockerModel)
+        );
+    }
+
 }
