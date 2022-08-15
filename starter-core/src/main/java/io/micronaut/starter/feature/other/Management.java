7f020e3615af1f94998bbed6584046aab15db5f4
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-15 10:49:26 +0200

    use MicronautDependencyUtils::coreDependency

--- starter-core/src/main/java/io/micronaut/starter/feature/other/Management.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/other/Management.java
@@ -18,6 +18,7 @@ package io.micronaut.starter.feature.other;
 import io.micronaut.starter.application.ApplicationType;
 import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
 import io.micronaut.starter.feature.Category;
 import io.micronaut.starter.feature.Feature;
 import io.micronaut.starter.feature.server.MicronautServerDependent;
@@ -59,8 +60,7 @@ public class Management implements Feature, MicronautServerDependent {
 
     @Override
     public void apply(GeneratorContext generatorContext) {
-        generatorContext.addDependency(Dependency.builder()
-                .groupId("io.micronaut")
+        generatorContext.addDependency(MicronautDependencyUtils.coreDependency()
                 .artifactId("micronaut-management")
                 .compile());
     }
