0c28bae75ea58621a050624b05272368a1eadf92
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-09-09 17:07:02 +0200

    Add object-storage-oracle-cloud

---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/objectstorage/ObjectStorageOracleCloudSpec.groovy
@@ -0,0 +1,67 @@
+package io.micronaut.starter.feature.objectstorage
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+
+class ObjectStorageOracleCloudSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    void "object-storage-oracle-cloud has the correct properties"() {
+        given:
+        ObjectStorageOracleCloud feature = beanContext.streamOfType(ObjectStorageOracleCloud).findFirst().orElse(null)
+
+        expect:
+        feature.name == "object-storage-oracle-cloud"
+        feature.title == "Object Storage - Oracle Cloud"
+        feature.description == "Micronaut Object Storage provides a uniform API to create, read and delete objects in the major cloud providers. This feature adds the Oracle Cloud implementation"
+        feature.category == Category.CLOUD
+        for (ApplicationType applicationType : ApplicationType.values()) {
+            assert feature.supports(applicationType)
+        }
+    }
+
+    void 'test #buildTool object-storage-oracle-cloud feature for language=#language'(Language language, BuildTool buildTool) {
+        when:
+        String template = new BuildBuilder(beanContext, buildTool)
+                .language(language)
+                .features(["object-storage-oracle-cloud"])
+                .render()
+
+        then:
+        template.contains('implementation("io.micronaut.oraclecloud:micronaut-oraclecloud-sdk")')
+        template.contains('implementation("io.micronaut.objectstorage:micronaut-object-storage-oracle-cloud")')
+
+        where:
+        [language, buildTool] << [Language.values().toList(), [BuildTool.GRADLE, BuildTool.GRADLE_KOTLIN]].combinations()
+    }
+
+    void 'test maven object-storage-oracle-cloud feature for language=#language'(Language language) {
+        when:
+        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+                .language(language)
+                .features(["object-storage-oracle-cloud"])
+                .render()
+
+        then:
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.oraclecloud</groupId>
+      <artifactId>micronaut-oraclecloud-sdk</artifactId>
+      <scope>compile</scope>
+    </dependency>
+    """)
+        template.contains("""
+    <dependency>
+      <groupId>io.micronaut.objectstorage</groupId>
+      <artifactId>micronaut-object-storage-oracle-cloud</artifactId>
+      <scope>compile</scope>
+    </dependency>
+""")
+        where:
+        language << Language.values().toList()
+    }
+}
