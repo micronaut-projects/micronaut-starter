f7e248adc85e360fbc38aae1b01d597bba7a24e4
sdelamo <sergio.delamo@softamo.com>
2022-09-28 08:02:53 +0200

    DataR2dbc does not support transactional

---
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/database/DataR2dbcSpec.groovy
@@ -0,0 +1,41 @@
+package io.micronaut.starter.core.test.feature.database
+
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.test.CommandSpec
+import org.gradle.testkit.runner.BuildResult
+import spock.lang.Unroll
+
+class DataR2dbcSpec extends CommandSpec {
+
+    @Override
+    String getTempDirectoryPrefix() {
+        return "dataR2dbc"
+    }
+
+    @Unroll
+    void "test maven data-r2dbc with #language"(Language language) {
+        when:
+        generateProject(language, BuildTool.MAVEN, ["data-r2dbc"])
+        String output = executeMaven("compile test")
+
+        then:
+        output?.contains("BUILD SUCCESS")
+
+        where:
+        language << Language.values()
+    }
+
+    @Unroll
+    void "test gradle data-r2dbc with #language"(Language language) {
+        when:
+        generateProject(language, BuildTool.GRADLE, ["data-r2dbc"])
+        BuildResult result = executeGradle("test")
+
+        then:
+        result?.output?.contains("BUILD SUCCESS")
+
+        where:
+        language << Language.values()
+    }
+}
