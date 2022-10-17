9d8fef0b46f6f50611022c69000984422c147300
Tim Yates <tim.yates@gmail.com>
2022-10-17 14:54:25 +0100

    Don't perform dependency substitutions if test-resources also applied
    If we do the dependency substitutions, then the test-resources server can not currently start
    up, and the task hangs waiting for success.
    
    For now, we will stop doing the substitution in Gradle if both features are applied as
    decided in https://github.com/micronaut-projects/micronaut-test-resources/issues/103

--- starter-core/src/main/java/io/micronaut/starter/feature/json/SerializationJsonpFeature.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/json/SerializationJsonpFeature.java
@@ -20,11 +20,13 @@ import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
 import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
 import io.micronaut.starter.build.dependencies.Substitution;
+import io.micronaut.starter.feature.testresources.TestResources;
 import io.micronaut.starter.options.BuildTool;
 import io.micronaut.starter.util.VersionInfo;
 import jakarta.inject.Singleton;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Singleton
@@ -66,6 +68,11 @@ public class SerializationJsonpFeature implements SerializationFeature {
     @Override
     @NonNull
     public List<Substitution> substitutions(@NonNull GeneratorContext generatorContext) {
+        // See https://github.com/micronaut-projects/micronaut-test-resources/issues/103
+        if (generatorContext.isFeaturePresent(TestResources.class)) {
+            return Collections.emptyList();
+        }
+
         String serializationVersion = VersionInfo.getBomVersion(MICRONAUT_SERIALIZATION);
         String dataBindVersion = generatorContext.resolveCoordinate("jakarta.json.bind-api").getVersion();
         return Arrays.asList(Substitution.builder()
