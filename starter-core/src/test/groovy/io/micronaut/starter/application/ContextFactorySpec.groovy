0d3505acdde3c6a25affaab7a68743fcb723de08
Sergio del Amo <sergio.delamo@softamo.com>
2022-10-18 07:16:18 +0200

    test: add test for sort

--- starter-core/src/test/groovy/io/micronaut/starter/application/ContextFactorySpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/application/ContextFactorySpec.groovy
@@ -1,8 +1,14 @@
 package io.micronaut.starter.application
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Replaces
 import io.micronaut.starter.feature.AvailableFeatures
-import io.micronaut.starter.feature.build.gradle.Gradle
+import io.micronaut.starter.feature.Feature
+import io.micronaut.starter.feature.FeatureContext
+import io.micronaut.starter.feature.test.Junit
+import io.micronaut.starter.feature.test.KoTest
+import io.micronaut.starter.feature.test.Mockk
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.JdkVersion
 import io.micronaut.starter.options.Language
@@ -11,12 +17,13 @@ import spock.lang.AutoCleanup
 import spock.lang.Issue
 import spock.lang.Shared
 import spock.lang.Specification
+import jakarta.inject.Singleton
 
 class ContextFactorySpec extends Specification {
 
     @Shared
     @AutoCleanup
-    ApplicationContext applicationContext = ApplicationContext.run()
+    ApplicationContext applicationContext = ApplicationContext.run(['spec.name': 'ContextFactorySpec'])
 
     @Shared
     AvailableFeatures availableFeatures = applicationContext.getBean(DefaultAvailableFeatures)
@@ -27,7 +34,7 @@ class ContextFactorySpec extends Specification {
     @Issue("https://github.com/micronaut-projects/micronaut-starter/issues/1473")
     void "avoid NPE by sorting default features"() {
         when:
-        contextFactory.createFeatureContext(availableFeatures,
+        FeatureContext featureContext = contextFactory.createFeatureContext(availableFeatures,
                 [],
                 ApplicationType.DEFAULT,
                 new Options(Language.KOTLIN, null, BuildTool.MAVEN, JdkVersion.JDK_17),
@@ -35,5 +42,33 @@ class ContextFactorySpec extends Specification {
         )
         then:
         noExceptionThrown()
+
+        when: 'testing features are applied before features that depend on them are present'
+        Set<Feature> selectedFeatures = featureContext.getSelectedFeatures()
+
+        then:
+        selectedFeatures.stream()
+                .anyMatch(f -> f.name == Mockk.NAME_MOCKK)
+    }
+
+    @Property(name = 'spec.name', value = 'ContextFactorySpec')
+    @Replaces(Junit.class)
+    @Singleton
+    static class JunitReplacement extends Junit {
+
+        @Override
+        boolean shouldApply(ApplicationType applicationType, Options options, Set<Feature> selectedFeatures) {
+            false
+        }
+    }
+
+    @Property(name = 'spec.name', value = 'ContextFactorySpec')
+    @Replaces(KoTest.class)
+    @Singleton
+    static class KoTestReplacement extends KoTest {
+        @Override
+        boolean shouldApply(ApplicationType applicationType, Options options, Set<Feature> selectedFeatures) {
+            true
+        }
     }
 }
