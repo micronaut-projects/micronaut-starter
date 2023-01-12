8b1f29197e303a3c9a0133b269aacc66f2379847
Tim Yates <tim.yates@gmail.com>
2023-01-12 10:21:56 +0000

    Fix ordering of R2dbc and DataR2dbc

--- test-features/src/test/groovy/io/micronaut/starter/core/test/feature/database/DataR2dbcSpec.groovy
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/database/DataR2dbcSpec.groovy
@@ -1,10 +1,10 @@
 package io.micronaut.starter.core.test.feature.database
 
+import io.micronaut.starter.feature.database.r2dbc.DataR2dbc
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.test.CommandSpec
 import org.gradle.testkit.runner.BuildResult
-import spock.lang.Unroll
 
 class DataR2dbcSpec extends CommandSpec {
 
@@ -13,10 +13,9 @@ class DataR2dbcSpec extends CommandSpec {
         return "dataR2dbc"
     }
 
-    @Unroll
     void "test maven data-r2dbc with #language"(Language language) {
         when:
-        generateProject(language, BuildTool.MAVEN, ["data-r2dbc"])
+        generateProject(language, BuildTool.MAVEN, [DataR2dbc.NAME])
         String output = executeMaven("compile test")
 
         then:
@@ -26,10 +25,9 @@ class DataR2dbcSpec extends CommandSpec {
         language << Language.values()
     }
 
-    @Unroll
     void "test gradle data-r2dbc with #language"(Language language) {
         when:
-        generateProject(language, BuildTool.GRADLE, ["data-r2dbc"])
+        generateProject(language, BuildTool.GRADLE, [DataR2dbc.NAME])
         BuildResult result = executeGradle("test")
 
         then:
