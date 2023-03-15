134b8c85afcc61f6187b0a4de5af2696a7b1e7a7
Tim Yates <tim.yates@gmail.com>
2023-03-15 10:25:29 +0000

    Use set comparison in BuildToolSpec

--- starter-core/src/test/groovy/io/micronaut/starter/options/BuildToolSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/options/BuildToolSpec.groovy
@@ -7,8 +7,6 @@ class BuildToolSpec extends Specification {
     void "valueGradle return Groovy and Kotlin DSL"() {
         expect:
         BuildTool.valuesGradle().stream().allMatch { it.isGradle() }
-        BuildTool.valuesGradle().stream().anyMatch { it == BuildTool.GRADLE_KOTLIN }
-        BuildTool.valuesGradle().stream().anyMatch { it == BuildTool.GRADLE }
-        [BuildTool.GRADLE, BuildTool.GRADLE_KOTLIN].size() == BuildTool.valuesGradle().size()
+        [BuildTool.GRADLE, BuildTool.GRADLE_KOTLIN] =~ BuildTool.valuesGradle()
     }
 }
