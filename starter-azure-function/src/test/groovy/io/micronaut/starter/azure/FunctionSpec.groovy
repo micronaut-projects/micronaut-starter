a6bda2ac6870f3af01c3cbec8de0bf13a24e4ced
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-20 10:34:52 +0200

    refactor test

--- starter-azure-function/src/test/groovy/io/micronaut/starter/azure/FunctionSpec.groovy
+++ starter-azure-function/src/test/groovy/io/micronaut/starter/azure/FunctionSpec.groovy
@@ -1,5 +1,11 @@
 package io.micronaut.starter.azure
 
+import com.microsoft.azure.functions.HttpResponseMessage
+import io.micronaut.azure.function.http.AzureHttpFunction
+import io.micronaut.azure.function.http.AzureHttpResponseMessage
+import io.micronaut.azure.function.http.AzureHttpResponseMessageAdapter
+import io.micronaut.azure.function.http.DefaultExecutionContext
+import io.micronaut.azure.function.http.HttpRequestMessageBuilder
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpMethod
 import io.micronaut.http.HttpStatus
@@ -10,23 +16,34 @@ import spock.lang.Specification
 class FunctionSpec extends Specification {
 
     void "test list features"() {
+        given:
+        Function function = new Function()
+
         when:
-        def function = new Function()
-        def response = function.request(HttpMethod.GET, '/application-types/default/features')
-                                .invoke()
+        AzureHttpResponseMessage responseMessage = invoke(function, function.request(HttpMethod.GET, '/application-types/default/features'))
+
         then:
-        response.status.value() == HttpStatus.OK.code
+        responseMessage.status.value() == HttpStatus.OK.code
     }
 
     void "test create app"() {
+        given:
+        Function function = new Function()
 
         when:
-        def function = new Function()
-        def response = function.request(HttpMethod.GET, "/create/default/test")
-                .invoke()
+        AzureHttpResponseMessage response = invoke(function, function.request(HttpMethod.GET, "/create/default/test"))
+
         then:
         response.status.value() == HttpStatus.CREATED.code
-        ZipUtil.isZip(response.body)
         response.getHeader(HttpHeaders.CONTENT_DISPOSITION).contains("test.zip")
+        ZipUtil.isZip(response.body.toString().bytes)
+    }
+
+    static AzureHttpResponseMessage invoke(AzureHttpFunction function, HttpRequestMessageBuilder<?> builder) {
+        HttpResponseMessage result = function.route(
+                builder.buildEncoded(),
+                new DefaultExecutionContext()
+        );
+        return new AzureHttpResponseMessageAdapter(result);
     }
 }
