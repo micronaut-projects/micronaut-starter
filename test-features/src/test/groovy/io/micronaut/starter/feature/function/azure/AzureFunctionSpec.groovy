8cc574af104e203d14b3fc26b434d287b53b66da
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-20 11:23:40 +0200

    ignore azure error
    Method too large: com/azure/resourcemanager/appservice/implementation/WebAppsClientImpl.$deserializeLambda\$ (Ljava/lang/invoke/SerializedLambda;)Ljava/lang/Object;

--- test-features/src/test/groovy/io/micronaut/starter/feature/function/azure/AzureFunctionSpec.groovy
+++ test-features/src/test/groovy/io/micronaut/starter/feature/function/azure/AzureFunctionSpec.groovy
@@ -3,10 +3,12 @@ package io.micronaut.starter.feature.function.azure
 import io.micronaut.starter.application.ApplicationType
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
+import spock.lang.Ignore
 import spock.lang.Unroll
 import io.micronaut.starter.test.CommandSpec
 import org.gradle.testkit.runner.BuildResult
 
+@Ignore('''Method too large: com/azure/resourcemanager/appservice/implementation/WebAppsClientImpl.$deserializeLambda\$ (Ljava/lang/invoke/SerializedLambda;)Ljava/lang/Object;''')
 class AzureFunctionSpec extends CommandSpec {
     @Override
     String getTempDirectoryPrefix() {
