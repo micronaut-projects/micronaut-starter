599ee255f920ff735842edc4038cbca29b54618e
sdelamo <sergio.delamo@softamo.com>
2023-04-24 09:23:30 +0200

    add test for validator

---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/build/KotlinSymbolProcessingValidatorSpec.groovy
@@ -0,0 +1,33 @@
+package io.micronaut.starter.feature.build
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.options.TestFramework
+
+class KotlinSymbolProcessingValidatorSpec  extends ApplicationContextSpec implements CommandOutputFixture {
+
+    void 'test feature ksp is only supported for Kotlin and Gradle'() {
+        String featureName = 'ksp'
+        when:
+        getFeatures([featureName], Language.GROOVY, TestFramework.JUNIT, BuildTool.GRADLE_KOTLIN)
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message.contains("Kotlin Symbol Processing (KSP) only supports Kotlin")
+
+        when:
+        getFeatures([featureName], Language.KOTLIN, TestFramework.JUNIT, BuildTool.MAVEN)
+
+        then:
+        ex = thrown()
+        ex.message.contains("Kotlin Symbol Processing (KSP) is only supported by Gradle")
+
+        when:
+        getFeatures([featureName], Language.KOTLIN, TestFramework.JUNIT, BuildTool.GRADLE)
+
+        then:
+        noExceptionThrown()
+    }
+}
