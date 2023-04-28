25d68049da4bd759f1ba47910691034d63af5478
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-28 16:55:45 +0200

    improve test-local.sh

--- test-local.sh
+++ test-local.sh
@@ -1,44 +1,60 @@
 #!/bin/bash
 EXIT_STATUS=0
 
-./gradlew check --refresh-dependencies --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$? 
+./gradlew --refresh-dependencies || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+./gradlew checkstyleMain --parallel --rerun-tasks || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+./gradlew :starter-core:build --parallel --rerun-tasks || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+./gradlew check --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-aws:test || EXIT_STATUS=$? 
+./gradlew test-aws:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-buildtool:test || EXIT_STATUS=$? 
+./gradlew test-buildtool:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-cli:test || EXIT_STATUS=$? 
+./gradlew test-cli:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-cloud:test || EXIT_STATUS=$? 
+./gradlew test-cloud:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-core:test || EXIT_STATUS=$? 
+./gradlew test-core:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-features:test  || EXIT_STATUS=$? 
+./gradlew test-features:test --parallel --rerun-tasks || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
-
-
