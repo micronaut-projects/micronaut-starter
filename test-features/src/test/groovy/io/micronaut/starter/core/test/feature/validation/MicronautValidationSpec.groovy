2a2f0479d1cfbbbbddbf967f94f41d3d927ff77d
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-28 11:01:14 +0200

    remove @PendingFeature

--- test-features/src/test/groovy/io/micronaut/starter/core/test/feature/validation/MicronautValidationSpec.groovy
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/validation/MicronautValidationSpec.groovy
@@ -5,7 +5,6 @@ import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.test.CommandSpec
 import io.micronaut.starter.test.LanguageBuildCombinations
-import spock.lang.PendingFeature
 
 class MicronautValidationSpec extends CommandSpec {
 
@@ -20,26 +19,7 @@ class MicronautValidationSpec extends CommandSpec {
         output.contains("BUILD SUCCESS")
 
         where:
-        [lang, buildTool] << LanguageBuildCombinations.combinations().findAll { !failingCombination(it) }
-    }
-
-    @PendingFeature
-    void 'test validation feature for #lang and #buildTool'(Language lang, BuildTool buildTool) {
-        given:
-        generateProject(lang, buildTool, [MicronautValidationFeature.NAME])
-
-        when:
-        String output = executeBuild(buildTool, "test")
-
-        then:
-        output.contains("BUILD SUCCESS")
-
-        where:
-        [lang, buildTool] << LanguageBuildCombinations.combinations().findAll { failingCombination(it) }
-    }
-
-    boolean failingCombination(List l) {
-        l[0] == Language.KOTLIN && l[1] == BuildTool.MAVEN
+        [lang, buildTool] << LanguageBuildCombinations.combinations()
     }
 
     @Override
