bc71df9f3d7bb62072c6c8170272b3860a48074e
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-01 13:39:26 +0200

    Ignore only Maven

--- test-features/src/test/groovy/io/micronaut/starter/core/test/feature/database/HibernateReactiveJpaSpec.groovy
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/database/HibernateReactiveJpaSpec.groovy
@@ -1,11 +1,7 @@
 package io.micronaut.starter.core.test.feature.database
 
-import io.micronaut.starter.feature.database.HibernateReactiveJpa
-import io.micronaut.starter.feature.database.MariaDB
-import io.micronaut.starter.feature.database.MySQL
-import io.micronaut.starter.feature.database.Oracle
-import io.micronaut.starter.feature.database.PostgreSQL
-import io.micronaut.starter.feature.database.SQLServer
+import io.micronaut.starter.core.test.feature.database.templates.book
+import io.micronaut.starter.feature.database.*
 import io.micronaut.starter.io.ConsoleOutput
 import io.micronaut.starter.io.FileSystemOutputHandler
 import io.micronaut.starter.options.BuildTool
@@ -14,12 +10,10 @@ import io.micronaut.starter.template.RockerWritable
 import io.micronaut.starter.test.BuildToolTest
 import io.micronaut.starter.test.CommandSpec
 import org.gradle.testkit.runner.BuildResult
-import io.micronaut.starter.core.test.feature.database.templates.book
 import spock.lang.Ignore
 import spock.lang.IgnoreIf
 import spock.lang.Requires
 
-@Ignore
 @Requires({ jvm.current.isJava11Compatible() })
 class HibernateReactiveJpaSpec extends CommandSpec {
     @Override
@@ -27,6 +21,7 @@ class HibernateReactiveJpaSpec extends CommandSpec {
         return "hibernateReactiveJpa"
     }
 
+    @Ignore('An exception java.lang.IncompatibleClassChangeError: class io.micronaut.http.filter.GenericHttpFilter$TerminalWithReactorContext cannot extend sealed interface io.micronaut.http.filter.GenericHttpFilter enable DEBUG level for full stacktrace was thrown by a user handler exceptionCaught() method while handling the following exception: java.lang.NoSuchFieldError: ROUTE')
     @IgnoreIf({ BuildToolTest.IGNORE_MAVEN })
     void "test maven hibernate-reactive-jpa with java and #db"(String db) {
         when:
