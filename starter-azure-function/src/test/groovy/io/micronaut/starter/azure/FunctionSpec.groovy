53291c67cae9f4f3a404a25a4dbab14e9ee98195
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-01 19:11:50 +0200

    test: remove @Ignore test passes

--- starter-azure-function/src/test/groovy/io/micronaut/starter/azure/FunctionSpec.groovy
+++ starter-azure-function/src/test/groovy/io/micronaut/starter/azure/FunctionSpec.groovy
@@ -10,17 +10,8 @@ import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpMethod
 import io.micronaut.http.HttpStatus
 import io.micronaut.starter.util.ZipUtil
-import spock.lang.Ignore
 import spock.lang.Specification
 
-@Ignore("""
-i.m.http.server.RouteExecutor - Unexpected error occurred: Receiver class io.micronaut.azure.function.http.AzureBinderRegistry does not define or inherit an implementation of the resolved method 'java.util.Optional findArgumentBinder(io.micronaut.core.type.Argument)' of interface io.micronaut.http.bind.RequestBinderRegistry.
-java.lang.AbstractMethodError: Receiver class io.micronaut.azure.function.http.AzureBinderRegistry does not define or inherit an implementation of the resolved method 'java.util.Optional findArgumentBinder(io.micronaut.core.type.Argument)' of interface io.micronaut.http.bind.RequestBinderRegistry.
-\tat io.micronaut.web.router.DefaultMethodBasedRouteInfo.resolveArgumentBindersInternal(DefaultMethodBasedRouteInfo.java:130)
-\tat io.micronaut.web.router.DefaultMethodBasedRouteInfo.resolveArgumentBinders(DefaultMethodBasedRouteInfo.java:116)
-\tat io.micronaut.web.router.AbstractRouteMatch.fulfillBeforeFilters(AbstractRouteMatch.java:294)
-\tat io.micronaut.http.server.binding.RequestArgumentSatisfier.fulfillArgumentRequirementsBeforeFilters(RequestArgumentSatisfier.java:57)
-""")
 class FunctionSpec extends Specification {
 
     void "test list features"() {
