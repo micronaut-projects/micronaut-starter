f9754d0d48f405b100a379ff4b5b31fe5b8036e1
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-03 18:18:08 +0200

    test local without build cache or predictive

--- test-local.sh
+++ test-local.sh
@@ -13,48 +13,48 @@ if [ $EXIT_STATUS -ne 0 ]; then
   exit $EXIT_STATUS
 fi
 
-./gradlew :starter-core:build --parallel --rerun-tasks || EXIT_STATUS=$?
+# PREDICTIVE_TEST_SELECTION=false ./gradlew :starter-core:build --parallel --rerun-tasks || EXIT_STATUS=$?
+# 
+# if [ $EXIT_STATUS -ne 0 ]; then
+#   exit $EXIT_STATUS
+# fi
+# 
+# PREDICTIVE_TEST_SELECTION=false ./gradlew check --no-build-cache --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$?
+# 
+# if [ $EXIT_STATUS -ne 0 ]; then  
+#   exit $EXIT_STATUS
+# fi
+# 
+# PREDICTIVE_TEST_SELECTION=false ./gradlew test-aws:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
+# 
+# if [ $EXIT_STATUS -ne 0 ]; then  
+#   exit $EXIT_STATUS
+# fi
 
-if [ $EXIT_STATUS -ne 0 ]; then
-  exit $EXIT_STATUS
-fi
-
-./gradlew check --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$?
-
-if [ $EXIT_STATUS -ne 0 ]; then  
-  exit $EXIT_STATUS
-fi
-
-./gradlew test-aws:test --parallel --rerun-tasks || EXIT_STATUS=$?
-
-if [ $EXIT_STATUS -ne 0 ]; then  
-  exit $EXIT_STATUS
-fi
-
-./gradlew test-buildtool:test --parallel --rerun-tasks || EXIT_STATUS=$?
-
-if [ $EXIT_STATUS -ne 0 ]; then  
-  exit $EXIT_STATUS
-fi
-
-./gradlew test-cli:test --parallel --rerun-tasks || EXIT_STATUS=$?
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-buildtool:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-cloud:test --parallel --rerun-tasks || EXIT_STATUS=$?
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-cli:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-core:test --parallel --rerun-tasks || EXIT_STATUS=$?
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-cloud:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then  
   exit $EXIT_STATUS
 fi
 
-./gradlew test-features:test --parallel --rerun-tasks || EXIT_STATUS=$?
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-core:test  --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then  
+  exit $EXIT_STATUS
+fi
+
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-features:test  --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
