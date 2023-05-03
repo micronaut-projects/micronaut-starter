bf26cc482f75d1cbf9575ef1e9438ef530c1ac3f
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-03 21:35:07 +0200

    uncomment sections of test-local.sh

--- test-local.sh
+++ test-local.sh
@@ -13,23 +13,23 @@ if [ $EXIT_STATUS -ne 0 ]; then
   exit $EXIT_STATUS
 fi
 
-# PREDICTIVE_TEST_SELECTION=false ./gradlew :starter-core:build --parallel --rerun-tasks || EXIT_STATUS=$?
-# 
-# if [ $EXIT_STATUS -ne 0 ]; then
-#   exit $EXIT_STATUS
-# fi
-# 
-# PREDICTIVE_TEST_SELECTION=false ./gradlew check --no-build-cache --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$?
-# 
-# if [ $EXIT_STATUS -ne 0 ]; then  
-#   exit $EXIT_STATUS
-# fi
-# 
-# PREDICTIVE_TEST_SELECTION=false ./gradlew test-aws:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
-# 
-# if [ $EXIT_STATUS -ne 0 ]; then  
-#   exit $EXIT_STATUS
-# fi
+PREDICTIVE_TEST_SELECTION=false ./gradlew :starter-core:build --parallel --rerun-tasks || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+PREDICTIVE_TEST_SELECTION=false ./gradlew check --no-build-cache --continue -x test-aws:test -x test-buildtool:test -x test-cli:test -x test-cloud:test -x test-core:test -x test-features:test || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then  
+  exit $EXIT_STATUS
+fi
+
+PREDICTIVE_TEST_SELECTION=false ./gradlew test-aws:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then  
+  exit $EXIT_STATUS
+fi
 
 PREDICTIVE_TEST_SELECTION=false ./gradlew test-buildtool:test --no-build-cache --parallel --rerun-tasks || EXIT_STATUS=$?
 
