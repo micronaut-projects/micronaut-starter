2abde354828cad98d7baaa2e3c19503ea0119e4b
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-04 15:43:45 +0200

    use Micronaut Application plugin

---
+++ starter-analytics-postgres/build.gradle.kts
@@ -0,0 +1,35 @@
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+
+plugins {
+    id("io.micronaut.internal.starter.convention")
+    id("io.micronaut.application")
+}
+
+dependencies {
+    annotationProcessor("io.micronaut.data:micronaut-data-processor")
+    annotationProcessor("io.micronaut:micronaut-graal")
+
+    implementation(project(":starter-core"))
+    implementation("io.micronaut:micronaut-http-server-netty")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("com.google.cloud.sql:postgres-socket-factory:1.11.1")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("io.micronaut.flyway:micronaut-flyway")
+    testCompileOnly("io.micronaut:micronaut-inject-groovy")
+    testImplementation("io.micronaut:micronaut-http-client")
+    testImplementation("io.micronaut.test:micronaut-test-spock")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("ch.qos.logback:logback-classic")
+}
+
+
+application {
+    mainClass.set("io.micronaut.starter.analytics.postgres.Main")
+}
+
+tasks.named<DockerBuildImage>("dockerBuild") {
+    images.add("gcr.io/${System.getenv("GCLOUD_PROJECT")}/micronaut-starter-analytics:snapshot")
+}
+
