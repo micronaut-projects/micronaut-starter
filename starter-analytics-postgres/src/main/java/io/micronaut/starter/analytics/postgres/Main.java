da96b9472d5cf2cf3de45a9cc7287c408b0def18
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-04 16:58:41 +0200

    enable deduce environment
    Attempt to fix https://github.com/micronaut-projects/micronaut-starter/issues/1769

--- starter-analytics-postgres/src/main/java/io/micronaut/starter/analytics/postgres/Main.java
+++ starter-analytics-postgres/src/main/java/io/micronaut/starter/analytics/postgres/Main.java
@@ -26,6 +26,6 @@ public class Main {
         System.setProperty("io.netty.machineId", MacAddressUtil.formatAddress(bestMacAddr));
     }
     public static void main(String... args) {
-        Micronaut.run(args);
+        Micronaut.build(args).deduceEnvironment(true).start();
     }
 }
