858947e71439f92d8a6fc024c78d662698132a92
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-02 18:19:24 +0200

    rewrite test to use new Lambda Handler
    refactor test

--- starter-aws-lambda/src/test/groovy/io/micronaut/starter/lambda/FunctionSpec.groovy
+++ starter-aws-lambda/src/test/groovy/io/micronaut/starter/lambda/FunctionSpec.groovy
@@ -1,15 +1,17 @@
 package io.micronaut.starter.lambda
 
-import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder
-import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse
+import com.amazonaws.services.lambda.runtime.ClientContext
+import com.amazonaws.services.lambda.runtime.CognitoIdentity
 import com.amazonaws.services.lambda.runtime.Context
-import com.fasterxml.jackson.databind.ObjectMapper
-import io.micronaut.function.aws.proxy.MicronautLambdaHandler
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import io.micronaut.core.type.Argument
+import io.micronaut.function.aws.proxy.payload1.ApiGatewayProxyRequestEventFunction
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpMethod
 import io.micronaut.http.HttpStatus
+import io.micronaut.json.JsonMapper
 import io.micronaut.starter.api.FeatureList
 import io.micronaut.starter.util.ZipUtil
 import spock.lang.AutoCleanup
@@ -22,25 +24,80 @@ class FunctionSpec extends Specification {
 
     @Shared
     @AutoCleanup
-    MicronautLambdaHandler handler = new MicronautLambdaHandler()
+    ApiGatewayProxyRequestEventFunction handler = new ApiGatewayProxyRequestEventFunction()
 
-    @Shared
-    ObjectMapper objectMapper = handler.applicationContext.getBean(ObjectMapper)
+    static final Context lambdaContext = new Context() {
 
-    @Shared Context lambdaContext = new MockLambdaContext()
+        @Override
+        String getAwsRequestId() {
+            return null
+        }
 
+        @Override
+        String getLogGroupName() {
+            return null
+        }
+
+        @Override
+        String getLogStreamName() {
+            return null
+        }
+
+        @Override
+        String getFunctionName() {
+            return null
+        }
+
+        @Override
+        String getFunctionVersion() {
+            return null
+        }
+
+        @Override
+        String getInvokedFunctionArn() {
+            return null
+        }
+
+        @Override
+        CognitoIdentity getIdentity() {
+            return null
+        }
+
+        @Override
+        ClientContext getClientContext() {
+            return null
+        }
+
+        @Override
+        int getRemainingTimeInMillis() {
+            return 0
+        }
+
+        @Override
+        int getMemoryLimitInMB() {
+            return 0
+        }
+
+        @Override
+        LambdaLogger getLogger() {
+            return null
+        }
+    }
     void "test list features"() {
         when:
-        AwsProxyRequest request = new AwsProxyRequestBuilder('/application-types/default/features', HttpMethod.GET.toString())
-                .build()
-        AwsProxyResponse response = handler.handleRequest(request, lambdaContext)
+        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
+        request.setPath('/application-types/default/features')
+        request.setHttpMethod(HttpMethod.GET.toString())
+
+        APIGatewayProxyResponseEvent response = handler.handleRequest(request, lambdaContext)
 
         then:
         response.statusCode == HttpStatus.OK.code
         response.body
 
         when:
-        FeatureList featureList = objectMapper.readValue(response.body, FeatureList)
+        JsonMapper jsonMapper = handler.getApplicationContext().getBean(JsonMapper.class)
+        FeatureList featureList = jsonMapper.readValue(response.body, Argument.of(FeatureList.class))
 
         then:
         !featureList.features.isEmpty()
@@ -48,13 +105,21 @@ class FunctionSpec extends Specification {
 
     void "test create app"() {
         when:
-        AwsProxyRequest request = new AwsProxyRequestBuilder('/create/default/test', HttpMethod.GET.toString()).build()
-        AwsProxyResponse response = handler.handleRequest(request, lambdaContext)
-        byte[] bytes = response.body.getBytes(StandardCharsets.UTF_8)
+        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent()
+        request.setPath('/create/default/test')
+        request.setHttpMethod(HttpMethod.GET.toString())
+        APIGatewayProxyResponseEvent response = handler.handleRequest(request, lambdaContext)
 
         then:
         response.statusCode == HttpStatus.CREATED.code
-        ZipUtil.isZip(Base64.mimeDecoder.decode(bytes))
-        response.multiValueHeaders.getFirst(HttpHeaders.CONTENT_DISPOSITION).contains("test.zip")
+        response.multiValueHeaders.get(HttpHeaders.CONTENT_DISPOSITION).stream().anyMatch {it.contains("test.zip") }
+
+        when:
+        byte[] bytes = response.body.getBytes(StandardCharsets.UTF_8)
+        boolean isZip = ZipUtil.isZip(Base64.mimeDecoder.decode(bytes))
+
+        then:
+        noExceptionThrown()
+        isZip
     }
 }
