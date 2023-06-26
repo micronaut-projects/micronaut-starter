ccbb78df196b2597b0b4066424d5a0a7ae99976f
sdelamo <sergio.delamo@softamo.com>
2023-06-26 08:24:58 +0200

    remove micronaut-runtime for Maven
    Close #1883

--- starter-core/src/test/groovy/io/micronaut/starter/feature/json/JsonFeatureSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/json/JsonFeatureSpec.groovy
@@ -2,10 +2,15 @@ package io.micronaut.starter.feature.json
 
 import io.micronaut.starter.ApplicationContextSpec
 import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.Scope
+import io.micronaut.starter.fixture.CommandOutputFixture
 import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
 import spock.lang.Unroll
 
-class JsonFeatureSpec extends ApplicationContextSpec {
+class JsonFeatureSpec extends ApplicationContextSpec implements CommandOutputFixture {
 
     @Unroll
     void "test selected Json feature is no longer preview: #feature.name"(SerializationFeature feature) {
@@ -17,109 +22,55 @@ class JsonFeatureSpec extends ApplicationContextSpec {
     }
 
     @Unroll
-    void "test selected JSON feature for Gradle: #feature"(String feature, String coordinate) {
+    void "test jackson-databind feature adds micronaut-jackson-databind-dependency for #buildTool "(BuildTool buildTool) {
+        given:
+        String groupId = 'io.micronaut'
+        String artifactId = 'micronaut-jackson-databind'
+        String feature = 'jackson-databind'
+        Language language = Language.JAVA
+
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
+        String template = new BuildBuilder(beanContext, buildTool)
                 .features([feature])
+                .language(language)
                 .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, template)
 
         then:
-        template.contains("implementation(\"$coordinate")
+        verifier.hasDependency(groupId, artifactId)
+        !verifier.hasDependency("micronaut-runtime")
 
         where:
-        coordinate                                | feature
-        'io.micronaut:micronaut-jackson-databind' | 'jackson-databind'
-    }
-
-    void "test selected JSON feature for Gradle: #feature and coordinate #module"(String module,
-                                                                                  String feature) {
-        when:
-        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
-                .features([feature])
-                .render()
-
-        then:
-        template.contains("implementation(\"$module")
-        template.contains('annotationProcessor("io.micronaut.serde:micronaut-serde-processor")')
-
-        where:
-        module                                       | feature
-        'io.micronaut.serde:micronaut-serde-jackson' | 'serialization-jackson'
-        'io.micronaut.serde:micronaut-serde-jsonp'   | 'serialization-jsonp'
-        'io.micronaut.serde:micronaut-serde-bson'    | 'serialization-bson'
-        'io.micronaut.serde:micronaut-serde-jackson' | 'serialization-jackson'
-        'io.micronaut.serde:micronaut-serde-jsonp'   | 'serialization-jsonp'
-        'io.micronaut.serde:micronaut-serde-bson'    | 'serialization-bson'
+        buildTool << BuildTool.values()
     }
 
     @Unroll
-    void "feature #feature for Maven adds dependency with artifact id: #artifactId"(String artifactId, String feature) {
+    void "test #feature feature adds serde processor and #artifactId dependency for #buildTool "(String feature, String artifactId, BuildTool buildTool) {
+        given:
+        String groupId = 'io.micronaut.serde'
+        Language language = Language.JAVA
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+        String template = new BuildBuilder(beanContext, buildTool)
                 .features([feature])
+                .language(language)
                 .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, template)
 
         then:
-        template.contains("<artifactId>$artifactId</artifactId>")
+        verifier.hasDependency("io.micronaut.serde", "micronaut-serde-processor", Scope.ANNOTATION_PROCESSOR)
+        verifier.hasDependency(groupId, artifactId)
+        !verifier.hasDependency("micronaut-runtime")
 
         where:
-        artifactId                   | feature
-        'micronaut-jackson-databind' | 'jackson-databind'
-    }
-
-    @Unroll
-    void "test selected JSON feature for Maven: #feature with test-resources #hasTestResources"(String artifactId, String feature) {
-        when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
-                .features([feature] + (hasTestResources ? ['test-resources'] : []))
-                .render()
-
-        then:
-        template.contains("<artifactId>$artifactId</artifactId>")
-        template.contains('<artifactId>micronaut-serde-processor</artifactId>')
-
-        and: "runtime is included and doesn't exclude jackson if test-resources is selected"
-        if (hasTestResources) {
-            assert template.contains("""\
-    <dependency>
-      <groupId>io.micronaut</groupId>
-      <artifactId>micronaut-runtime</artifactId>
-      <scope>compile</scope>
-    </dependency>""")
-        } else {
-            assert template.contains("""\
-    <dependency>
-      <groupId>io.micronaut</groupId>
-      <artifactId>micronaut-runtime</artifactId>
-      <scope>compile</scope>
-      <exclusions>
-          <exclusion>
-            <groupId>io.micronaut</groupId>
-            <artifactId>micronaut-jackson-databind</artifactId>
-          </exclusion>
-        </exclusions>
-    </dependency>""")
-        }
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.serde</groupId>
-      <artifactId>$artifactId</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
-        if (feature == 'serialization-jsonp') {
-            assert template.contains("""\
-    <dependency>
-      <groupId>jakarta.json.bind</groupId>
-      <artifactId>jakarta.json.bind-api</artifactId>""")
-        }
-        where:
-        artifactId                | feature                 | hasTestResources
-        'micronaut-serde-jackson' | 'serialization-jackson' | false
-        'micronaut-serde-jsonp'   | 'serialization-jsonp'   | false
-        'micronaut-serde-bson'    | 'serialization-bson'    | false
-        'micronaut-serde-jackson' | 'serialization-jackson' | true
-        'micronaut-serde-jsonp'   | 'serialization-jsonp'   | true
-        'micronaut-serde-bson'    | 'serialization-bson'    | true
+        feature                 | artifactId                | buildTool
+        'serialization-jackson' | 'micronaut-serde-jackson' | BuildTool.GRADLE_KOTLIN
+        'serialization-jackson' | 'micronaut-serde-jackson' | BuildTool.GRADLE
+        'serialization-jackson' | 'micronaut-serde-jackson' | BuildTool.MAVEN
+        'serialization-jsonp'   | 'micronaut-serde-jsonp'   | BuildTool.GRADLE_KOTLIN
+        'serialization-jsonp'   | 'micronaut-serde-jsonp'   | BuildTool.GRADLE
+        'serialization-jsonp'   | 'micronaut-serde-jsonp'   | BuildTool.MAVEN
+        'serialization-bson'    | 'micronaut-serde-bson'    | BuildTool.GRADLE_KOTLIN
+        'serialization-bson'    | 'micronaut-serde-bson'    | BuildTool.GRADLE
+        'serialization-bson'    | 'micronaut-serde-bson'    | BuildTool.MAVEN
     }
 }
