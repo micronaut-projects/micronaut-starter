830e6e941d962d2179499f896a3d683c3409e60f
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-05 08:33:12 +0200

    Fix Zipkin dependency

--- starter-core/src/main/java/io/micronaut/starter/feature/tracing/Zipkin.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/tracing/Zipkin.java
@@ -17,13 +17,15 @@ package io.micronaut.starter.feature.tracing;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.starter.application.generator.GeneratorContext;
-import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
 import io.micronaut.starter.feature.server.MicronautServerDependent;
 
 import jakarta.inject.Singleton;
 
 @Singleton
 public class Zipkin implements TracingFeature, MicronautServerDependent {
+
+    private static final String ARTIFACT_ID_MICRONAUT_TRACING_BRAVE_HTTP = "micronaut-tracing-brave-http";
 
     @NonNull
     @Override
@@ -44,15 +46,16 @@ public class Zipkin implements TracingFeature, MicronautServerDependent {
 
     @Override
     public void apply(GeneratorContext generatorContext) {
-
-        generatorContext.addDependency(Dependency.builder()
-                .groupId("io.micronaut.tracing")
-                .artifactId("micronaut-tracing-zipkin")
-                .compile());
-
+        addDependencies(generatorContext);
         generatorContext.getConfiguration().put("tracing.zipkin.enabled", true);
         generatorContext.getConfiguration().put("tracing.zipkin.http.url", "http://localhost:9411");
         generatorContext.getConfiguration().put("tracing.zipkin.sampler.probability", 0.1);
+    }
+
+    protected void addDependencies(GeneratorContext generatorContext) {
+        generatorContext.addDependency(MicronautDependencyUtils.tracingDependency()
+                .artifactId(ARTIFACT_ID_MICRONAUT_TRACING_BRAVE_HTTP)
+                .compile());
     }
 
     @Override
