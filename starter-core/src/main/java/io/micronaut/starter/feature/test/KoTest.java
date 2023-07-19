9695fbd1f97647830024613cc44f8fcef23dcd9b
Dean Wette <wetted@unityfoundation.io>
2023-07-19 06:19:57 -0500

    Drop Kotest 4 in favor of Kotest 5. (#1965)
    * Drop Kotest 4 in favor of Kotest 5.
    
    closes #1614
    
    * Fix kotest5 imports
    
    * fix issue with Kotest feature Maven dependencies
    
    * fix test

--- starter-core/src/main/java/io/micronaut/starter/feature/test/KoTest.java
+++ starter-core/src/main/java/io/micronaut/starter/feature/test/KoTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 original authors
+ * Copyright 2017-2023 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,21 +19,21 @@ import io.micronaut.core.annotation.NonNull;
 import io.micronaut.starter.application.generator.GeneratorContext;
 import io.micronaut.starter.build.dependencies.Dependency;
 import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
-import io.micronaut.starter.options.BuildTool;
 import io.micronaut.starter.options.TestFramework;
 import io.micronaut.starter.template.URLTemplate;
-
 import jakarta.inject.Singleton;
+
+import static io.micronaut.starter.options.BuildTool.MAVEN;
 
 @Singleton
 public class KoTest implements TestFeature {
-    protected static final String ARTIFACT_ID_MICRONAUT_TEST_KOTEST = "micronaut-test-kotest";
 
-    protected static final Dependency DEPENDENCY_MICRONAUT_TEST_KOTEST = MicronautDependencyUtils
+    protected static final Dependency DEPENDENCY_MICRONAUT_TEST_KOTEST5 = MicronautDependencyUtils
             .testDependency()
-            .artifactId(ARTIFACT_ID_MICRONAUT_TEST_KOTEST)
+            .artifactId("micronaut-test-kotest5")
             .test()
             .build();
+
     protected static final String ARTIFACT_ID_KOTEST_RUNNER_JUNIT_5_JVM = "kotest-runner-junit5-jvm";
 
     protected static final String ARTIFACT_ID_KOTEST_ASSERTIONS_CORE_JVM = "kotest-assertions-core-jvm";
@@ -52,8 +52,8 @@ public class KoTest implements TestFeature {
                         classLoader.getResource("kotest/ProjectConfig.kt")));
 
         // Only for Maven, these dependencies are applied by the Micronaut Gradle Plugin
-        if (generatorContext.getBuildTool() == BuildTool.MAVEN) {
-            generatorContext.addDependency(DEPENDENCY_MICRONAUT_TEST_KOTEST);
+        if (generatorContext.getBuildTool() == MAVEN) {
+            generatorContext.addDependency(DEPENDENCY_MICRONAUT_TEST_KOTEST5);
             generatorContext.addDependency(Dependency.builder()
                     .lookupArtifactId(ARTIFACT_ID_KOTEST_RUNNER_JUNIT_5_JVM)
                     .test());
@@ -68,4 +68,18 @@ public class KoTest implements TestFeature {
         return TestFramework.KOTEST;
     }
 
+    @Override
+    public String getTitle() {
+        return "Micronaut Test Kotest5";
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-test/latest/guide/#kotest5";
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://kotest.io/";
+    }
 }
