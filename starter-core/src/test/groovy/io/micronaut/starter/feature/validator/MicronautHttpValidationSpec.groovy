edb14966004b121e1f04ae747e3d3ebe8c1ab50c
Guillermo Calvo <guillermo@guillermo.dev>
2023-07-20 17:55:03 +0200

    Add unit test to check version used by micronaut-http-validation

--- starter-core/src/test/groovy/io/micronaut/starter/feature/validator/MicronautHttpValidationSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/validator/MicronautHttpValidationSpec.groovy
@@ -3,8 +3,10 @@ package io.micronaut.starter.feature.validator
 import io.micronaut.starter.ApplicationContextSpec
 import io.micronaut.starter.BuildBuilder
 import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.application.generator.GeneratorContext
 import io.micronaut.starter.build.BuildTestUtil
 import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.Dependency
 import io.micronaut.starter.build.dependencies.Scope
 import io.micronaut.starter.feature.Category
 import io.micronaut.starter.options.BuildTool
@@ -42,5 +44,25 @@ class MicronautHttpValidationSpec  extends ApplicationContextSpec {
 
         where:
         buildTool << BuildTool.values()
+    }
+
+    void "dependency added for micronaut-http-validation is aligned with micronaut core version"() {
+        given:
+        GeneratorContext context = Mock()
+        List<Dependency> dependencies = []
+
+        when:
+        micronautHttpValidation.apply(context)
+
+        then:
+        1 * context.addDependency(_) >> (args) -> { dependencies << args[0] }
+        0 * _
+
+        and:
+        dependencies.stream().anyMatch {
+            it.artifactId == "micronaut-http-validation" &&
+            it.versionProperty == "micronaut.core.version" &&
+            it.scope == Scope.ANNOTATION_PROCESSOR
+        }
     }
 }
