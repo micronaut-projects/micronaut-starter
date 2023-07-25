b849691c96ed41ce0f33636641784ca20d1c4314
Guillermo Calvo <guillermo@guillermo.dev>
2023-07-25 17:32:07 +0200

    Remove unused import

--- starter-analytics-postgres/src/main/java/io/micronaut/starter/analytics/postgres/AnalyticsController.java
+++ starter-analytics-postgres/src/main/java/io/micronaut/starter/analytics/postgres/AnalyticsController.java
@@ -20,7 +20,6 @@ import io.micronaut.data.model.Pageable;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
