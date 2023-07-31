9595a99b75d5b236dc7f96eecb5ac2cece8b3250
Guillermo Calvo <guillermo@guillermo.dev>
2023-07-31 13:08:27 +0200

    Set `micronaut.starter.analytics.page-size` to test pagination logic

--- starter-analytics-postgres/src/test/groovy/io/micronaut/starter/analytics/postgres/ExcelGeneratorSpec.groovy
+++ starter-analytics-postgres/src/test/groovy/io/micronaut/starter/analytics/postgres/ExcelGeneratorSpec.groovy
@@ -32,7 +32,8 @@ class ExcelGeneratorSpec extends Specification implements TestPropertyProvider {
     Map<String, String> getProperties() {
         postgres.start()
 
-        ["datasources.default.url":postgres.getJdbcUrl(),
+        ["micronaut.starter.analytics.page-size":5,
+         "datasources.default.url":postgres.getJdbcUrl(),
          "datasources.default.username":postgres.getUsername(),
          "datasources.default.password":postgres.getPassword(),
          "datasources.default.dialect": Dialect.POSTGRES.name()]
@@ -88,12 +89,36 @@ class ExcelGeneratorSpec extends Specification implements TestPropertyProvider {
         applicationRepository.deleteAll()
     }
 
-    void "generate spreadsheet with two applications"() {
+    void "generate spreadsheet with 12 applications"() {
         given: "there are two applications stored in the repository"
         List<Application> apps = applicationRepository.saveAll([
-                new Application(ApplicationType.FUNCTION, Language.KOTLIN, BuildTool.GRADLE_KOTLIN, TestFramework.KOTEST, JdkVersion.JDK_17, "1.2.3"),
-                new Application(ApplicationType.CLI, Language.GROOVY, BuildTool.MAVEN, TestFramework.JUNIT, JdkVersion.JDK_20, "3.2.1")])
-        featureRepository.saveAll([new Feature(apps[0], "hello"), new Feature(apps[1], "world")])
+                new Application(ApplicationType.FUNCTION, Language.KOTLIN, BuildTool.GRADLE_KOTLIN, TestFramework.KOTEST, JdkVersion.JDK_17, "4.0.1"),
+                new Application(ApplicationType.CLI, Language.GROOVY, BuildTool.MAVEN, TestFramework.JUNIT, JdkVersion.JDK_20, "4.0.2"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.3"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.4"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.5"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.6"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.7"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.8"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.9"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.10"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.11"),
+                new Application(ApplicationType.DEFAULT, Language.JAVA, BuildTool.GRADLE, TestFramework.SPOCK, JdkVersion.JDK_17, "4.0.12"),
+        ])
+        featureRepository.saveAll([
+                new Feature(apps[0], "one"),
+                new Feature(apps[1], "two"),
+                new Feature(apps[2], "three"),
+                new Feature(apps[3], "four"),
+                new Feature(apps[4], "five"),
+                new Feature(apps[5], "six"),
+                new Feature(apps[6], "seven"),
+                new Feature(apps[7], "eight"),
+                new Feature(apps[8], "nine"),
+                new Feature(apps[9], "ten"),
+                new Feature(apps[10], "eleven"),
+                new Feature(apps[11], "twelve"),
+        ])
         when: "the spreadsheet is generated"
         Workbook workbook = excelGenerator.generateExcel()
 
@@ -101,7 +126,7 @@ class ExcelGeneratorSpec extends Specification implements TestPropertyProvider {
         workbook.numberOfSheets == 1
         with(workbook.getSheetAt(0)) {
             sheetName == ExcelGenerator.SHEET_NAME
-            physicalNumberOfRows == 3
+            physicalNumberOfRows == 13
             with(getRow(0)) {
                 rowStyle.locked == true
                 getCell(ID.ordinal()).stringCellValue == ID.title
@@ -116,25 +141,65 @@ class ExcelGeneratorSpec extends Specification implements TestPropertyProvider {
             }
             with(getRow(1)) {
                 getCell(ID.ordinal()).numericCellValue == apps[0].id
-                getCell(FEATURES.ordinal()).stringCellValue == "hello"
+                getCell(FEATURES.ordinal()).stringCellValue == "one"
                 getCell(TYPE.ordinal()).stringCellValue == "FUNCTION"
                 getCell(LANGUAGE.ordinal()).stringCellValue == "kotlin"
                 getCell(BUILD_TOOL.ordinal()).stringCellValue == "gradle_kotlin"
                 getCell(TEST_FRAMEWORK.ordinal()).stringCellValue == "kotest"
                 getCell(JDK_VERSION.ordinal()).numericCellValue == 17
-                getCell(MICRONAUT_VERSION.ordinal()).stringCellValue == "1.2.3"
+                getCell(MICRONAUT_VERSION.ordinal()).stringCellValue == "4.0.1"
                 getCell(DATE_CREATED.ordinal()).numericCellValue == DateUtil.getExcelDate(apps[0].dateCreated, false)
             }
             with(getRow(2)) {
                 getCell(ID.ordinal()).numericCellValue == apps[1].id
-                getCell(FEATURES.ordinal()).stringCellValue == "world"
+                getCell(FEATURES.ordinal()).stringCellValue == "two"
                 getCell(TYPE.ordinal()).stringCellValue == "CLI"
                 getCell(LANGUAGE.ordinal()).stringCellValue == "groovy"
                 getCell(BUILD_TOOL.ordinal()).stringCellValue == "maven"
                 getCell(TEST_FRAMEWORK.ordinal()).stringCellValue == "junit"
                 getCell(JDK_VERSION.ordinal()).numericCellValue == 20
-                getCell(MICRONAUT_VERSION.ordinal()).stringCellValue == "3.2.1"
+                getCell(MICRONAUT_VERSION.ordinal()).stringCellValue == "4.0.2"
                 getCell(DATE_CREATED.ordinal()).numericCellValue == DateUtil.getExcelDate(apps[1].dateCreated, false)
+            }
+            with(getRow(3)) {
+                getCell(ID.ordinal()).numericCellValue == apps[2].id
+                getCell(FEATURES.ordinal()).stringCellValue == "three"
+            }
+            with(getRow(4)) {
+                getCell(ID.ordinal()).numericCellValue == apps[3].id
+                getCell(FEATURES.ordinal()).stringCellValue == "four"
+            }
+            with(getRow(5)) {
+                getCell(ID.ordinal()).numericCellValue == apps[4].id
+                getCell(FEATURES.ordinal()).stringCellValue == "five"
+            }
+            with(getRow(6)) {
+                getCell(ID.ordinal()).numericCellValue == apps[5].id
+                getCell(FEATURES.ordinal()).stringCellValue == "six"
+            }
+            with(getRow(7)) {
+                getCell(ID.ordinal()).numericCellValue == apps[6].id
+                getCell(FEATURES.ordinal()).stringCellValue == "seven"
+            }
+            with(getRow(8)) {
+                getCell(ID.ordinal()).numericCellValue == apps[7].id
+                getCell(FEATURES.ordinal()).stringCellValue == "eight"
+            }
+            with(getRow(9)) {
+                getCell(ID.ordinal()).numericCellValue == apps[8].id
+                getCell(FEATURES.ordinal()).stringCellValue == "nine"
+            }
+            with(getRow(10)) {
+                getCell(ID.ordinal()).numericCellValue == apps[9].id
+                getCell(FEATURES.ordinal()).stringCellValue == "ten"
+            }
+            with(getRow(11)) {
+                getCell(ID.ordinal()).numericCellValue == apps[10].id
+                getCell(FEATURES.ordinal()).stringCellValue == "eleven"
+            }
+            with(getRow(12)) {
+                getCell(ID.ordinal()).numericCellValue == apps[11].id
+                getCell(FEATURES.ordinal()).stringCellValue == "twelve"
             }
         }
         cleanup:
