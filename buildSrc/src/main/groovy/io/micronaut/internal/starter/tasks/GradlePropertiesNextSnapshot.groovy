c00ad2515c014be6dc0cebbe2d6ef7b2ce93cc66
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-04 10:24:11 +0200

    return core and platform to snapshot after release (#1995)
    Close #1986

---
+++ buildSrc/src/main/groovy/io/micronaut/internal/starter/tasks/GradlePropertiesNextSnapshot.groovy
@@ -0,0 +1,37 @@
+package io.micronaut.internal.starter.tasks
+
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+
+
+abstract class GradlePropertiesNextSnapshot extends DefaultTask {
+    @Input
+    abstract Property<String> getPropertyName()
+
+    @InputFile
+    abstract RegularFileProperty getGradleProperties()
+
+    GradlePropertiesNextSnapshot() {
+        getGradleProperties().convention(getProject().getLayout().getProjectDirectory().file("gradle.properties"))
+    }
+
+    @TaskAction
+    def changePropertiesFile() {
+        File propertiesFile = getGradleProperties().get().asFile
+        Properties properties = new Properties()
+        propertiesFile.withInputStream {
+            properties.load(it)
+        }
+        String key = propertyName.get()
+        String version = properties.get(key)
+        SoftwareVersion softwareVersion = SoftwareVersion.build(version)
+        if (softwareVersion.nextSnapshot()) {
+            properties.setProperty(key, softwareVersion.toString())
+            properties.store(new FileOutputStream(propertiesFile), "")
+        }
+
+    }
+}
