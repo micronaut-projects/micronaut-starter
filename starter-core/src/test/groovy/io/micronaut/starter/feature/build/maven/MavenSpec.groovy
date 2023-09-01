1672ec2e79ba59ca3755a770f699859190225395
Sergio del Amo <sergio.delamo@softamo.com>
2023-09-01 12:36:08 +0200

    build: groovy 4.0.14

--- starter-core/src/test/groovy/io/micronaut/starter/feature/build/maven/MavenSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/build/maven/MavenSpec.groovy
@@ -165,7 +165,7 @@ class MavenSpec extends ApplicationContextSpec implements CommandOutputFixture {
       <artifactId>groovy</artifactId>
 ''')
         template.contains('''\
-    <groovyVersion>4.0.13</groovyVersion>
+    <groovyVersion>4.0.14</groovyVersion>
 ''')
     }
 
