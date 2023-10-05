f2233bfb31bc77405f99e2dcf46dccbd22aef74a
Tim Yates <tim.yates@gmail.com>
2023-10-05 11:48:52 +0100

    Fix Swagger (#2106)
    * Fix Swagger
    
    We seem to be missing the static resource mapping for swagger and rapidoc.
    
    This change adds the 2 static-resource mappings in to the config so they work again.
    
    Run locally with `./gradlew :s-w-n:run` and then navigate to
    
    http://localhost:8080/rapidoc/index.html
    http://localhost:8080/swagger-ui/index.html
    
    * Add tests and fix for Jetty

---
+++ starter-web-servlet/src/test/groovy/io/micronaut/starter/servlet/OpenAPISpec.groovy
@@ -0,0 +1,60 @@
+package io.micronaut.starter.servlet
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+@MicronautTest
+class OpenAPISpec extends Specification {
+
+    @Inject
+    EmbeddedServer server
+
+    @Client("/")
+    @Inject
+    HttpClient client
+
+    void "test swagger-ui"() {
+        when:
+        def response = server.getURI().resolve("/swagger-ui").toURL().text
+
+        List<String> matches = (response =~ /(?:src|href)=['"]([^'"]+)['"]/).collect { it[1] as String }
+        String yml = (response =~ /['"]([^'"]+\.yml)['"]/)[0][1]
+
+        then:
+        matches.size()
+        yml
+
+        and:
+        server.getURI().resolve(yml).toURL().text.contains("title: Micronaut Launch")
+
+        and:
+        matches.each {
+            assert client.toBlocking().exchange(it).status() == HttpStatus.OK
+        }
+    }
+
+    void "test rapidoc"() {
+        when:
+        def response = server.getURI().resolve("/rapidoc").toURL().text
+
+        List<String> matches = (response =~ /(?:src|href)=['"]([^'"]+)['"]/).collect { it[1] as String }
+        String yml = (response =~ /['"]([^'"]+\.yml)['"]/)[0][1]
+
+        then:
+        matches.size()
+        yml
+
+        and:
+        server.getURI().resolve(yml).toURL().text.contains("title: Micronaut Launch")
+
+        and:
+        matches.each {
+            assert client.toBlocking().exchange(it).status() == HttpStatus.OK
+        }
+    }
+}
