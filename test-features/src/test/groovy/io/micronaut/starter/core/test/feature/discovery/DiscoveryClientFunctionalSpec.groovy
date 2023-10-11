945b43fec2887a7296196932042f0857903cf056
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-11 12:15:51 +0200

    fix: http client as compile for discovery-client (#2125)
    * add tests
    
    * fix: http client as compile for discovery-client
    
    * Update test-features/src/test/groovy/io/micronaut/starter/core/test/feature/discovery/DiscoveryClientFunctionalSpec.groovy
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>
    
    * Update test-features/src/test/groovy/io/micronaut/starter/core/test/feature/discovery/DiscoveryClientFunctionalSpec.groovy
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

---
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/discovery/DiscoveryClientFunctionalSpec.groovy
@@ -0,0 +1,28 @@
+package io.micronaut.starter.core.test.feature.discovery
+
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.test.CommandSpec
+import io.micronaut.starter.test.LanguageBuildCombinations
+
+class DiscoveryClientFunctionalSpec extends CommandSpec {
+
+    void 'test discovery-client for #lang and #buildTool'(Language lang, BuildTool buildTool) {
+        given:
+        generateProject(lang, buildTool, ['discovery-client'])
+
+        when:
+        String output = executeBuild(buildTool, "test")
+
+        then:
+        output.contains("BUILD SUCCESS")
+
+        where:
+        [lang, buildTool] << LanguageBuildCombinations.combinations()
+    }
+
+    @Override
+    String getTempDirectoryPrefix() {
+        return "test-app-discovery-client"
+    }
+}
