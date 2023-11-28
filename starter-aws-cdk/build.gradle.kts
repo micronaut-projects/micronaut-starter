8a059b240b76a75b64569d187b7c039aa020ffc2
Dean Wette <wetted@unityfoundation.io>
2023-11-28 13:26:43 -0600

    Update starter lambda features to support 21
    closes #2199

--- starter-aws-cdk/build.gradle.kts
+++ starter-aws-cdk/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 val micronautVersion: String by project
 
 dependencies {
-    api("software.amazon.awscdk:aws-cdk-lib:2.93.0")
+    api("software.amazon.awscdk:aws-cdk-lib:2.110.1")
     api(project(":starter-core"))
     testImplementation(platform("io.micronaut.platform:micronaut-platform:$micronautVersion"))
     testImplementation("io.micronaut.aws:micronaut-function-aws-api-proxy")
