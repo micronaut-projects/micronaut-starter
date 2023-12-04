7306fd9e0cee00dc7801d3e6cb4c6b0cd82035ee
Dean Wette <wetted@unityfoundation.io>
2023-12-04 10:32:00 -0600

    Add micronaut-openapi-adoc feature (asciidoc conversion).
    closes #2206

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/other/OpenApiAdoc.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.other;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.feature.FeatureContext;
+import io.micronaut.starter.feature.server.MicronautServerDependent;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class OpenApiAdoc implements Feature, MicronautServerDependent {
+
+    public static final String NAME = "openapi-adoc";
+
+    private static final Dependency DEPENDENCY_OPENAPI_ASCIIDOC = MicronautDependencyUtils
+            .openapi()
+            .artifactId("micronaut-openapi-adoc")
+            .annotationProcessor()
+            .build();
+
+    private final OpenApi openApiFeature;
+
+    public OpenApiAdoc(OpenApi openApiFeature) {
+        this.openApiFeature = openApiFeature;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getTitle() {
+        return "OpenAPI Conversion to Asciidoc";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds and enables document conversion to asciidoc";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.getConfiguration().put("micronaut.openapi.adoc.enabled", true);
+        generatorContext.addDependency(DEPENDENCY_OPENAPI_ASCIIDOC);
+    }
+
+    @Override
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        featureContext.addFeatureIfNotPresent(OpenApi.class, openApiFeature);
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return applicationType == ApplicationType.DEFAULT;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.API;
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/other/OpenApiAdocSpec.groovy
@@ -0,0 +1,54 @@
+package io.micronaut.starter.feature.other
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.application.generator.GeneratorContext
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.feature.Features
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.options.TestFramework
+import spock.lang.Unroll
+
+class OpenApiAdocSpec extends ApplicationContextSpec  implements CommandOutputFixture {
+
+    void "openapi-adoc feature includes openapi feature"() {
+        when:
+        Features features = getFeatures(['openapi-adoc'])
+
+        then:
+        features.contains('openapi')
+    }
+
+    void "openapi-adoc belongs to API category"() {
+        expect:
+        Category.API == beanContext.getBean(OpenApiAdoc).category
+    }
+
+    @Unroll
+    void 'test openapi-adoc with #buildTool for language=#language'(Language language, BuildTool buildTool) {
+        when:
+        String template = new BuildBuilder(beanContext, buildTool)
+                .features(['openapi-adoc'])
+                .language(language)
+                .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, TestFramework.JUNIT, template)
+
+        then:
+        verifier.hasAnnotationProcessor('io.micronaut.openapi', 'micronaut-openapi-adoc')
+
+        where:
+        [language, buildTool] << [Language.values(), BuildTool.values()].combinations()
+    }
+
+    void "test config enables openapi-adoc"() {
+        when:
+        GeneratorContext ctx = buildGeneratorContext(['openapi-adoc'])
+
+        then:
+        ctx.configuration.get('micronaut.openapi.adoc.enabled') == true
+    }
+}
