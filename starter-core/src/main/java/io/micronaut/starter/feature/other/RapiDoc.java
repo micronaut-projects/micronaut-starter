39558d52e860e09d207896b63ba3b58697389fb7
Dean Wette <wetted@unityfoundation.io>
2023-12-06 12:43:02 -0600

    Add RapiDoc view feature for OpenApi.
    closes #2205

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/other/RapiDoc.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.other;
+
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.feature.FeatureContext;
+import io.micronaut.starter.feature.github.workflows.WorkflowsUtils;
+import io.micronaut.starter.feature.other.template.openApiRapiDocProperties;
+import io.micronaut.starter.feature.security.Security;
+import io.micronaut.starter.feature.server.MicronautServerDependent;
+import io.micronaut.starter.template.RockerTemplate;
+import jakarta.inject.Singleton;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+public class RapiDoc implements Feature, MicronautServerDependent {
+    public static final String NAME = "rapidoc";
+
+    private final OpenApi openApiFeature;
+
+    public RapiDoc(OpenApi openApiFeature) {
+        this.openApiFeature = openApiFeature;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getTitle() {
+        return "RapiDoc View";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds and enables RapiDoc view for OpenApi";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return applicationType == ApplicationType.DEFAULT;
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addTemplate("openapiRapiDocProperties", new RockerTemplate("openapi.properties", openApiRapiDocProperties.template()));
+        generatorContext.getConfiguration().put("micronaut.router.static-resources.swagger.paths", "classpath:META-INF/swagger");
+        generatorContext.getConfiguration().put("micronaut.router.static-resources.swagger.mapping", "/swagger/**");
+        generatorContext.getConfiguration().put("micronaut.router.static-resources.rapidoc.paths", "classpath:META-INF/swagger/views/rapidoc");
+        generatorContext.getConfiguration().put("micronaut.router.static-resources.rapidoc.mapping", "/rapidoc/**");
+
+        if (generatorContext.isFeaturePresent(Security.class)) {
+            Map<String, String> swaggerAccess = new HashMap<>() {{
+                put("pattern", "/swagger/**");
+                put("access", "isAnonymous()");
+            }};
+
+            Map<String, String> swaggerUiAccess = new HashMap<>() {{
+                put("pattern", "/swagger-ui/**");
+                put("access", "isAnonymous()");
+            }};
+
+            generatorContext.getConfiguration().put("micronaut.security.intercept-url-map", Arrays.asList(swaggerAccess, swaggerUiAccess));
+        }
+
+        generatorContext.addTemplate("exampleController", WorkflowsUtils.createExampleController(
+                generatorContext.getProject(), generatorContext.getLanguage()));
+
+    }
+
+    @Override
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        featureContext.addFeatureIfNotPresent(OpenApi.class, openApiFeature);
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.API;
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://rapidocweb.com/api.html";
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/other/RapiDocSpec.groovy
@@ -0,0 +1,82 @@
+package io.micronaut.starter.feature.other
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.application.generator.GeneratorContext
+import io.micronaut.starter.feature.Features
+import io.micronaut.starter.fixture.CommandOutputFixture
+
+class RapiDocSpec extends ApplicationContextSpec  implements CommandOutputFixture {
+
+    void "test rapidoc features"() {
+        when:
+        Features features = getFeatures(['rapidoc'])
+
+        then:
+        features.contains("openapi")
+    }
+
+    void "test config without security feature"() {
+        when:
+        GeneratorContext ctx = buildGeneratorContext(['rapidoc'])
+        def output = generate(ApplicationType.DEFAULT, ctx)
+
+        then:
+        ctx.configuration.get('micronaut.router.static-resources.swagger.paths') == "classpath:META-INF/swagger"
+        ctx.configuration.get('micronaut.router.static-resources.swagger.mapping') == "/swagger/**"
+        ctx.configuration.get('micronaut.router.static-resources.rapidoc.paths') == "classpath:META-INF/swagger/views/rapidoc"
+        ctx.configuration.get('micronaut.router.static-resources.rapidoc.mapping') == "/rapidoc/**"
+
+        output["openapi.properties"].readLines()[0] == "swagger-ui.enabled=false"
+        output["openapi.properties"].readLines()[1] == "redoc.enabled=false"
+        output["openapi.properties"].readLines()[2] == "rapidoc.enabled=true"
+        output["openapi.properties"].readLines()[3] == "rapidoc.bg-color=#14191f"
+        output["openapi.properties"].readLines()[4] == "rapidoc.text-color=#aec2e0"
+        output["openapi.properties"].readLines()[5] == "rapidoc.sort-endpoints-by=method"
+
+        output.containsKey("src/main/java/example/micronaut/FooController.java")
+        output.containsKey("src/test/java/example/micronaut/FooTest.java")
+
+    }
+
+    void "test config with security feature"() {
+        when:
+        GeneratorContext ctx = buildGeneratorContext(['rapidoc', 'security'])
+        def output = generate(ApplicationType.DEFAULT, ctx)
+
+        then:
+        ctx.configuration.get('micronaut.router.static-resources.swagger.paths') == "classpath:META-INF/swagger"
+        ctx.configuration.get('micronaut.router.static-resources.swagger.mapping') == "/swagger/**"
+        ctx.configuration.get('micronaut.router.static-resources.rapidoc.paths') == "classpath:META-INF/swagger/views/rapidoc"
+        ctx.configuration.get('micronaut.router.static-resources.rapidoc.mapping') == "/rapidoc/**"
+
+        List<Map<String, String>> swaggerSec = ctx.configuration.get('micronaut.security.intercept-url-map') as List<Map<String, String>>
+
+        swaggerSec.get(0).get("access") == "isAnonymous()"
+        swaggerSec.get(0).get("pattern") == "/swagger/**"
+
+        swaggerSec.get(1).get("access") == "isAnonymous()"
+        swaggerSec.get(1).get("pattern") == "/swagger-ui/**"
+
+        output["openapi.properties"].readLines()[0] == "swagger-ui.enabled=false"
+        output["openapi.properties"].readLines()[1] == "redoc.enabled=false"
+        output["openapi.properties"].readLines()[2] == "rapidoc.enabled=true"
+        output["openapi.properties"].readLines()[3] == "rapidoc.bg-color=#14191f"
+        output["openapi.properties"].readLines()[4] == "rapidoc.text-color=#aec2e0"
+        output["openapi.properties"].readLines()[5] == "rapidoc.sort-endpoints-by=method"
+
+        output.containsKey("src/main/java/example/micronaut/FooController.java")
+        output.containsKey("src/test/java/example/micronaut/FooTest.java")
+
+    }
+
+    void "test rapidoc has third part docs"() {
+        when:
+        GeneratorContext ctx = buildGeneratorContext(['rapidoc'])
+        def output = generate(ApplicationType.DEFAULT, ctx)
+
+        then:
+        output["README.md"].contains("https://rapidocweb.com/api.html")
+    }
+
+}
