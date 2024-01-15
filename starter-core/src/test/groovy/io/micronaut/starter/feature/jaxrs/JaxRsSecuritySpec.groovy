cc4ed3d8ebb6337db8a324924af63789f12959ea
Dean Wette <wetted@unityfoundation.io>
2024-01-15 13:47:46 -0600

    Remove duplicate annotation processor dependencies in gradle/maven projects
    fixes #2264

--- starter-core/src/test/groovy/io/micronaut/starter/feature/jaxrs/JaxRsSecuritySpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/jaxrs/JaxRsSecuritySpec.groovy
@@ -2,9 +2,12 @@ package io.micronaut.starter.feature.jaxrs
 
 import io.micronaut.starter.ApplicationContextSpec
 import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
 import io.micronaut.starter.fixture.CommandOutputFixture
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
+import spock.lang.Issue
 import spock.lang.Unroll
 
 class JaxRsSecuritySpec extends ApplicationContextSpec implements CommandOutputFixture {
@@ -26,11 +29,12 @@ class JaxRsSecuritySpec extends ApplicationContextSpec implements CommandOutputF
                 .features(['security', JaxRs.NAME, 'kapt'])
                 .language(language)
                 .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(BuildTool.GRADLE, language, template)
 
         then:
-        template.contains('implementation("io.micronaut.jaxrs:micronaut-jaxrs-server-security")')
-        template.contains('implementation("io.micronaut.jaxrs:micronaut-jaxrs-server")')
-        template.contains("$scope(\"io.micronaut.jaxrs:micronaut-jaxrs-processor\")")
+        verifier.hasDependency("io.micronaut.jaxrs", "micronaut-jaxrs-server-security")
+        verifier.hasDependency("io.micronaut.jaxrs", "micronaut-jaxrs-server")
+        verifier.hasDependency("io.micronaut.jaxrs", "micronaut-jaxrs-processor", scope)
 
         where:
         language        | scope
@@ -50,93 +54,44 @@ class JaxRsSecuritySpec extends ApplicationContextSpec implements CommandOutputF
         !template.contains('implementation("io.micronaut.jaxrs:micronaut-jaxrs-server-security")')
     }
 
-    void 'test maven jax-rs-security feature'() {
+    void 'test maven jax-rs-security feature for language=#language'() {
         when:
         String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+                .language(language)
                 .features(['security', JaxRs.NAME])
                 .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(BuildTool.MAVEN, language, template)
 
         then:
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server-security</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
+        verifier.hasDependency("io.micronaut.jaxrs", "micronaut-jaxrs-server-security")
+        verifier.hasDependency("io.micronaut.jaxrs", "micronaut-jaxrs-server")
+        verifier.hasAnnotationProcessor("io.micronaut.jaxrs", "micronaut-jaxrs-processor")
 
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
-        template.contains("""
-            <path>
-              <groupId>io.micronaut.jaxrs</groupId>
-              <artifactId>micronaut-jaxrs-processor</artifactId>
-              <version>\${micronaut.jaxrs.version}</version>
-              <exclusions>
-                <exclusion>
-                  <groupId>io.micronaut</groupId>
-                  <artifactId>micronaut-inject</artifactId>
-                </exclusion>
-              </exclusions>
-            </path>
-""")
+        and:
+        if (language == Language.KOTLIN) {
+            assert verifier.hasTestAnnotationProcessor("io.micronaut.jaxrs", "micronaut-jaxrs-processor")
+        }
+        where:
+        language << Language.values()
+    }
 
+    @Issue("https://github.com/micronaut-projects/micronaut-starter/issues/2264")
+    void "test maven jax-rs feature for Groovy doesn't duplicate annotation processors"() {
         when:
-        template = new BuildBuilder(beanContext, BuildTool.MAVEN)
-                .language(Language.KOTLIN)
-                .features(['security', JaxRs.NAME])
-                .render()
-
-        then:
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server-security</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
-
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
-        template.count('''\
-               <annotationProcessorPath>
-                 <groupId>io.micronaut.jaxrs</groupId>
-                 <artifactId>micronaut-jaxrs-processor</artifactId>
-                 <version>${micronaut.jaxrs.version}</version>
-               </annotationProcessorPath>
-''') == 2
-
-        when:
-        template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
                 .language(Language.GROOVY)
                 .features(['security', JaxRs.NAME])
                 .render()
 
         then:
-        template.contains("""
+        // duplicates were added for Groovy annotationProcessor scope, so avoid regression
+        template.count('''\
     <dependency>
       <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server-security</artifactId>
-      <scope>compile</scope>
+      <artifactId>micronaut-jaxrs-processor</artifactId>
+      <scope>provided</scope>
     </dependency>
-""")
-
-        template.contains("""
-    <dependency>
-      <groupId>io.micronaut.jaxrs</groupId>
-      <artifactId>micronaut-jaxrs-server</artifactId>
-      <scope>compile</scope>
-    </dependency>
-""")
+''') == 1
     }
+
 }
