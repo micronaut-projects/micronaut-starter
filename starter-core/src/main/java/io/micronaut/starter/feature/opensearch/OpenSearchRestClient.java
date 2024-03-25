d095288ad1dc6f61f26786bd46eaad4db610c975
Dean Wette <wetted@unityfoundation.io>
2024-03-25 14:43:27 -0500

    Add OpenSearch features.
    closes #2414

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/opensearch/OpenSearchRestClient.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.opensearch;
+
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
+import io.micronaut.starter.feature.FeatureContext;
+import io.micronaut.starter.feature.json.JacksonDatabindFeature;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class OpenSearchRestClient implements OpenSearchFeature {
+
+    public static final String NAME = "opensearch-restclient";
+
+    private final JacksonDatabindFeature jacksonDatabindFeature;
+    private final Dependency jacksonDatabindDependency = MicronautDependencyUtils
+            .coreDependency()
+            .artifactId("micronaut-jackson-databind")
+            .compile()
+            .build();
+
+    public OpenSearchRestClient(JacksonDatabindFeature jacksonDatabindFeature) {
+        this.jacksonDatabindFeature = jacksonDatabindFeature;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getTitle() {
+        return "OpenSearch REST Client";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        OpenSearchFeature.super.apply(generatorContext);
+    }
+
+    @Override
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        if (!featureContext.isPresent(JacksonDatabindFeature.class)) {
+            featureContext.addFeature(jacksonDatabindFeature);
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for OpenSearch RestClient-based transport";
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/opensearch/OpenSearchFeatureSpec.groovy
@@ -0,0 +1,49 @@
+package io.micronaut.starter.feature.opensearch
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+
+class OpenSearchFeatureSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    void 'test opensearch feature #opensearchFeature.name contributes dependencies for #buildTool'(OpenSearchFeature opensearchFeature, BuildTool buildTool) {
+        given:
+        String template = new BuildBuilder(beanContext, buildTool)
+                .features([opensearchFeature.name])
+                .render()
+        when:
+        String groupId = MicronautDependencyUtils.GROUP_ID_MICRONAUT_OPENSEARCH
+        String artifactId = "micronaut-" + opensearchFeature.getName()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, template)
+
+        then:
+        verifier.hasDependency(groupId, artifactId)
+        if (opensearchFeature instanceof OpenSearchRestClient) {
+            assert verifier.hasDependency("io.micronaut", "micronaut-jackson-databind")
+        }
+
+        where:
+        [opensearchFeature, buildTool] << [beanContext.getBeansOfType(OpenSearchFeature), BuildTool.values()].combinations()
+    }
+
+    void "test opensearch feature #opensearchFeature.name is client category"(OpenSearchFeature opensearchFeature) {
+        expect:
+        Category.CLIENT == opensearchFeature.getCategory()
+        where:
+        [opensearchFeature, buildTool] << [beanContext.getBeansOfType(OpenSearchFeature), BuildTool.values()].combinations()
+    }
+
+    void "test opensearch feature #opensearchFeature.name documentation links"(OpenSearchFeature opensearchFeature) {
+        expect:
+        opensearchFeature.getMicronautDocumentation() == 'https://micronaut-projects.github.io/micronaut-opensearch/latest/guide/'
+        opensearchFeature.getThirdPartyDocumentation() == 'https://opensearch.org/docs/latest/clients/java/'
+
+        where:
+        [opensearchFeature, buildTool] << [beanContext.getBeansOfType(OpenSearchFeature), BuildTool.values()].combinations()
+    }
+}
