573a5d6d5a263feeba7ba91d5696fb798f43bd92
Dean Wette <wetted@unityfoundation.io>
2024-04-10 11:54:35 -0500

    add json-schema-validation feature

---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/json/JsonSchemaValidationFeatureSpec.groovy
@@ -0,0 +1,87 @@
+package io.micronaut.starter.feature.json
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.application.ApplicationType
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.Scope
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import spock.lang.Shared
+import spock.lang.Subject
+
+class JsonSchemaValidationFeatureSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    @Shared
+    @Subject
+    JsonSchemaValidationFeature jsonSchemaValidationFeature = beanContext.getBean(JsonSchemaValidationFeature)
+
+    void 'readme.md with feature json-schema-validation contains links to docs'() {
+        when:
+        Map<String, String> output = generate([JsonSchemaFeature.NAME, JsonSchemaValidationFeature.NAME])
+        String readme = output["README.md"]
+
+        then:
+        readme
+        readme.contains("https://micronaut-projects.github.io/micronaut-json-schema/latest/guide/index.html#validation")
+    }
+
+    void "json-schema-validation belongs to API category"() {
+        expect:
+        Category.API == jsonSchemaValidationFeature.category
+    }
+
+    void "json-schema-validation application type = #applicationType"(ApplicationType applicationType) {
+        expect:
+        jsonSchemaValidationFeature.supports(applicationType)
+
+        where:
+        applicationType << ApplicationType.values()
+    }
+
+    void "json-schema-validation feature adds test dependency for language=#language buildTool=#buildTool "(BuildTool buildTool, Language language) {
+        when:
+        String template = new BuildBuilder(beanContext, buildTool)
+                .features([JsonSchemaFeature.NAME, JsonSchemaValidationFeature.NAME])
+                .language(language)
+                .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, template)
+
+        then:
+        template
+        verifier.hasDependency("io.micronaut.json-schema", "micronaut-json-schema-validation", Scope.TEST)
+
+        where:
+        [buildTool, language] << [BuildTool.values(), Language.values()].combinations()
+    }
+
+    void "adding json-schema-validation feature is error is jason-schema not present "() {
+        when:
+        new BuildBuilder(beanContext, BuildTool.DEFAULT_OPTION)
+                .features([JsonSchemaValidationFeature.NAME])
+                .render()
+
+        then:
+        def e = thrown(IllegalArgumentException)
+        e.message == 'json-schema-validation requires json-schema'
+
+        when:
+        new BuildBuilder(beanContext, BuildTool.DEFAULT_OPTION)
+                .features([JsonSchemaFeature.NAME, JsonSchemaValidationFeature.NAME])
+                .render()
+
+        then:
+        noExceptionThrown()
+
+        when:
+        new BuildBuilder(beanContext, BuildTool.DEFAULT_OPTION)
+                .features([JsonSchemaFeature.NAME])
+                .render()
+
+        then:
+        noExceptionThrown()
+    }
+}
