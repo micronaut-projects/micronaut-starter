01b959b9bae1cb7fa65d379f9c1a3080284bfe6b
sdelamo <sergio.delamo@softamo.com>
2024-04-11 07:38:38 +0200

    split java 21 and java 17 methods

--- test-core/src/test/groovy/io/micronaut/starter/core/test/GradleDockerConfigSpec.groovy
+++ test-core/src/test/groovy/io/micronaut/starter/core/test/GradleDockerConfigSpec.groovy
@@ -4,15 +4,16 @@ import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.JdkVersion
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.test.CommandSpec
+import org.gradle.testkit.runner.BuildResult
+import spock.lang.Requires
 
 class GradleDockerConfigSpec extends CommandSpec {
 
-    void "basic dockerfile creation"(BuildTool buildTool, Integer javaVersion) {
+    @Requires({ javaVersion == 17 })
+    void "Java 17 basic dockerfile creation"(BuildTool buildTool, Integer javaVersion) {
         when:
         generateProjectForVersion(Language.JAVA, JdkVersion.valueOf(javaVersion), buildTool)
-
-        // dockerfileNative depends on compileJava so we need to exclude it (as if we're on Java 17, we can't compile a java 21 build)
-        def result = executeGradle("dockerfile", "dockerfileNative", "-x", "compileJava")
+        BuildResult result = executeGradle("dockerfile", "dockerfileNative")
 
         then:
         result.output.contains("BUILD SUCCESS")
@@ -20,27 +21,36 @@ class GradleDockerConfigSpec extends CommandSpec {
         new File(dir, "build/docker/native-main/DockerfileNative").text.contains("FROM ghcr.io/graalvm/native-image-community:$javaVersion-ol9 AS graalvm")
 
         where:
-        [buildTool, javaVersion] << [
-                BuildTool.valuesGradle(),
-                [17, 21]
-        ].combinations()
-        dockerBaseImage = javaVersion == 17 ? "eclipse-temurin:17-jre" : "eclipse-temurin:21-jre-jammy"
+        [buildTool, javaVersion] << [BuildTool.valuesGradle(), [17]].combinations()
+        dockerBaseImage = "eclipse-temurin:17-jre"
+    }
+
+    @Requires({ javaVersion == 21 })
+    void "Java 21 basic dockerfile creation"(BuildTool buildTool, Integer javaVersion) {
+        when:
+        generateProjectForVersion(Language.JAVA, JdkVersion.valueOf(javaVersion), buildTool)
+        BuildResult result = executeGradle("dockerfile", "dockerfileNative")
+
+        then:
+        result.output.contains("BUILD SUCCESS")
+        new File(dir, "build/docker/main/Dockerfile").text.contains("FROM $dockerBaseImage")
+        new File(dir, "build/docker/native-main/DockerfileNative").text.contains("FROM ghcr.io/graalvm/native-image-community:$javaVersion-ol9 AS graalvm")
+
+        where:
+        [buildTool, javaVersion] << [BuildTool.valuesGradle(), [21]].combinations()
+        dockerBaseImage = "eclipse-temurin:21-jre-jammy"
     }
 
     void "test dockerfiles can be built"(BuildTool buildTool, String command) {
         when:
         generateProject(Language.JAVA, buildTool)
-
-        def result = executeGradle(command)
+        BuildResult result = executeGradle(command)
 
         then:
         result.output.contains("BUILD SUCCESS")
 
         where:
-        [buildTool, command] << [
-                BuildTool.valuesGradle(),
-                ['dockerBuild', 'dockerBuildNative']
-        ].combinations()
+        [buildTool, command] << [BuildTool.valuesGradle(), ['dockerBuild', 'dockerBuildNative']].combinations()
     }
 
     @Override
