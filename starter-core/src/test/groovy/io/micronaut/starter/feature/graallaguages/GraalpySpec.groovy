d4d13313ccca40f1b773f14c22167a233e655d1a
Tomas Stupka <tomas.stupka@oracle.com>
2025-03-31 16:06:20 +0200

    added support for graalpy gradle plugin (#2708)
    * added support for graalpy gradle plugin
    
    * supply Kotlin Dsl to rocker template
    
    ---------
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- starter-core/src/test/groovy/io/micronaut/starter/feature/graallaguages/GraalpySpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/graallaguages/GraalpySpec.groovy
@@ -23,15 +23,18 @@ class GraalpySpec extends ApplicationContextSpec implements CommandOutputFixture
     @Subject
     Graalpy micronautGraalPyFeature = beanContext.getBean(Graalpy)
 
-    void 'readme.md with feature micronaut-graalpy contains links to docs'() {
+    void "readme.md with feature micronaut-graalpy contains links to docs for language=#language buildTool=#buildTool "(BuildTool buildTool, Language language) {
         when:
-        Map<String, String> output = generate(ApplicationType.DEFAULT, new Options(Language.JAVA, TestFramework.JUNIT, BuildTool.MAVEN, JdkVersion.JDK_21), [Graalpy.NAME])
+        Map<String, String> output = generate(ApplicationType.DEFAULT, new Options(language, TestFramework.JUNIT, buildTool, JdkVersion.JDK_21), [Graalpy.NAME])
         String readme = output["README.md"]
 
         then:
         readme
         readme.contains("https://micronaut-projects.github.io/micronaut-graal-languages/latest/guide");
         readme.contains("https://graalvm.org/python");
+
+        where:
+        [buildTool, language] << [BuildTool.values(), Language.JAVA].combinations()
     }
 
     void "micronaut-graalpy belongs to LANGUAGES category"() {
@@ -47,29 +50,25 @@ class GraalpySpec extends ApplicationContextSpec implements CommandOutputFixture
         applicationType << ApplicationType.values()
     }
 
-    void "micronaut-graalpy feature adds micronaut-graalpy dependency for Java and Maven "() {
+    void "micronaut-graalpy feature adds micronaut-graalpy dependency for language=#language buildTool=#buildTool "(BuildTool buildTool, Language language) {
         when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+        String template = new BuildBuilder(beanContext, buildTool)
                 .features([Graalpy.NAME])
-                .language(Language.JAVA)
+                .language(language)
                 .render()
-        BuildTestVerifier verifier = BuildTestUtil.verifier(BuildTool.MAVEN, Language.JAVA, template)
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, template)
 
         then:
         template
         verifier.hasDependency("io.micronaut.graal-languages", "micronaut-graalpy", Scope.COMPILE)
-    }
+        if (buildTool == BuildTool.MAVEN) {
+            assert template.contains("<artifactId>graalpy-maven-plugin</artifactId>")
+        } else if (buildTool.isGradle()) {
+            assert verifier.hasBuildPlugin("org.graalvm.python")
+        }
 
-    void "micronaut-graalpy feature adds maven-graalvm-plugin for language=java buildTool=maven "() {
-        when:
-        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
-                .features([Graalpy.NAME])
-                .language(Language.JAVA)
-                .render()
-
-        then:
-        template
-        template.contains("<artifactId>graalpy-maven-plugin</artifactId>")
+        where:
+        [buildTool, language] << [BuildTool.values(), Language.JAVA].combinations()
     }
 
     void "micronaut-graalpy feature requires java 21"() {
@@ -84,14 +83,4 @@ class GraalpySpec extends ApplicationContextSpec implements CommandOutputFixture
         ex.message == "The selected feature graalpy requires at latest Java 21"
     }
 
-    void 'test feature graalpy is only supported for Maven'() {
-        given:
-        String featureName = 'graalpy'
-        when:
-        getFeatures([featureName], new Options(Language.JAVA, TestFramework.JUNIT, BuildTool.GRADLE, JdkVersion.JDK_21))
-
-        then:
-        IllegalArgumentException ex = thrown()
-        ex.message.contains("Feature only supported by Maven")
-    }
 }
