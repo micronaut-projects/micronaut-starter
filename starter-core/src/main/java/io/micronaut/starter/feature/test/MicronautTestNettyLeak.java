9457bbb1437f9a3d0bf4198b752772d41eb95363
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-20 11:34:08 +0200

    feature: micronaut test netty leak

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/test/MicronautTestNettyLeak.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.test;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.Dependency;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
+import io.micronaut.starter.feature.Category;
+import io.micronaut.starter.feature.DefaultFeature;
+import io.micronaut.starter.feature.Feature;
+import io.micronaut.starter.options.Options;
+import io.micronaut.starter.options.TestFramework;
+import jakarta.inject.Singleton;
+
+import java.util.Set;
+
+@Requires(property = "micronaut.starter.feature.micronaut-test-netty-leak.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
+@Singleton
+public class MicronautTestNettyLeak implements DefaultFeature {
+    private static final Dependency DEPENDENCY_MICRONAUT_TEST_NETTY_LEAK = MicronautDependencyUtils.testDependency()
+            .artifactId("micronaut-test-netty-leak")
+            .testRuntime()
+            .build();
+
+    @Override
+    @NonNull
+    public String getName() {
+        return "test-netty-leak";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Micronaut Test Netty Leak";
+    }
+
+    @Override
+    @NonNull
+    public String getDescription() {
+        return "Light-weight Netty leak detection mechanism specifically designed for tests";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.TEST;
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://netty.io/4.2/api/io/netty/util/LeakPresenceDetector.html";
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-test/latest/guide/#nettyLeak";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        addDependency(generatorContext);
+    }
+
+    protected void addDependency(GeneratorContext generatorContext) {
+        generatorContext.addDependency(DEPENDENCY_MICRONAUT_TEST_NETTY_LEAK);
+    }
+
+    @Override
+    public boolean shouldApply(ApplicationType applicationType, Options options, Set<Feature> selectedFeatures) {
+        return options.getTestFramework() == TestFramework.JUNIT || options.getTestFramework() == TestFramework.SPOCK;
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/test/MicronautTestNettyLeakSpec.groovy
@@ -0,0 +1,67 @@
+package io.micronaut.starter.feature.test
+
+import io.micronaut.core.util.StringUtils
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.Scope
+import io.micronaut.starter.feature.Category
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import io.micronaut.starter.options.TestFramework
+import spock.lang.Shared
+import spock.lang.Subject
+import spock.lang.Unroll
+
+class MicronautTestNettyLeakSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    static final String FEATURE = "test-netty-leak"
+
+    @Shared
+    @Subject
+    MicronautTestNettyLeak featureSubject = beanContext.getBean(MicronautTestNettyLeak)
+
+    void 'test readme.md with feature test-netty-leak contains links to docs'() {
+        when:
+        Map<String, String> output = generate([FEATURE])
+        String readme = output["README.md"]
+
+        then:
+        readme
+        readme.contains("https://netty.io/4.2/api/io/netty/util/LeakPresenceDetector.html")
+        readme.contains("https://micronaut-projects.github.io/micronaut-test/latest/guide/#nettyLeak")
+    }
+
+    void "test test-netty-leak belongs to Test category"() {
+        expect:
+        Category.TEST == featureSubject.category
+    }
+
+    @Unroll
+    void 'test gradle test-netty-leak feature for language: #language buildTool: #buildTool testFramework: #testFramework and feature: #featureName'(
+            String featureName,
+            Language language,
+            BuildTool buildTool,
+            TestFramework testFramework) {
+        when:
+        String template = new BuildBuilder(beanContext, buildTool)
+                .features(StringUtils.isEmpty(featureName) ? [] : [featureName])
+                .language(language)
+                .testFramework(testFramework)
+                .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(buildTool, language, testFramework, template)
+
+        then:
+        verifier.hasDependency("io.micronaut.test", "micronaut-test-netty-leak", Scope.TEST_RUNTIME)
+
+        where:
+        [featureName, language, buildTool, testFramework] << [
+                [FEATURE, ''], // use an empty feature name to test default inclusion
+                Language.values().toList(),
+                BuildTool.values().toList(),
+                [TestFramework.JUNIT, TestFramework.SPOCK]
+        ].combinations()
+    }
+}
