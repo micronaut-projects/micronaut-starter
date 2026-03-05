c94aa9edac3281d0fe967c82160017302e7fc38a
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-05 15:35:49 +0100

    remove import

--- starter-core/src/test/groovy/io/micronaut/starter/feature/chatbots/telegram/TelegramAzureChatBotSpec.groovy
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/chatbots/telegram/TelegramAzureChatBotSpec.groovy
@@ -7,7 +7,6 @@ import io.micronaut.starter.feature.function.azure.AzureBuildCommandUtils
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
 import io.micronaut.starter.options.Options
-import spock.lang.Shared
 
 class TelegramAzureChatBotSpec extends BaseTelegramChatBotSpec {
 
