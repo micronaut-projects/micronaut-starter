cbf34cacd73049290c27b364210351501b2cfab3
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-12 11:55:56 +0100

    changes to ProjectConfig in KoTest 6

--- starter-core/src/main/resources/kotest/ProjectConfig.kt
+++ starter-core/src/main/resources/kotest/ProjectConfig.kt
@@ -4,5 +4,5 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 
 object ProjectConfig : AbstractProjectConfig() {
-    override fun extensions() = listOf(MicronautKotest5Extension)
+    override val extensions = listOf(MicronautKotest5Extension)
 }
