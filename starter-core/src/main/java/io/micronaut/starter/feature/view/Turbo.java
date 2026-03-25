5bbae53307738f042f08e82f136674598ff5d379
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-25 10:03:20 +0100

    add turbo feature

---
+++ starter-core/src/main/java/io/micronaut/starter/feature/view/Turbo.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.starter.feature.view;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.starter.application.generator.GeneratorContext;
+import io.micronaut.starter.build.dependencies.MicronautDependencyUtils;
+import io.micronaut.starter.feature.server.MicronautServerDependent;
+import jakarta.inject.Singleton;
+
+@Requires(property = "micronaut.starter.feature.views.turbo.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
+@Singleton
+public class Turbo implements ViewFeature, MicronautServerDependent {
+
+    public static final String ARTIFACT_ID_MICRONAUT_VIEWS_TURBO = "micronaut-views-turbo";
+
+    @Override
+    public String getName() {
+        return "turbo";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Turbo";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds support for Turbo";
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://turbo.hotwired.dev/";
+    }
+
+    @Override
+    public String getMicronautDocumentation() {
+        return "https://micronaut-projects.github.io/micronaut-views/latest/guide/#turbo";
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addDependency(MicronautDependencyUtils.viewsDependency()
+                .artifactId(ARTIFACT_ID_MICRONAUT_VIEWS_TURBO)
+                .compile());
+    }
+}
---
+++ starter-core/src/test/groovy/io/micronaut/starter/feature/view/TurboSpec.groovy
@@ -0,0 +1,56 @@
+package io.micronaut.starter.feature.view
+
+import io.micronaut.starter.ApplicationContextSpec
+import io.micronaut.starter.BuildBuilder
+import io.micronaut.starter.build.BuildTestUtil
+import io.micronaut.starter.build.BuildTestVerifier
+import io.micronaut.starter.build.dependencies.Scope
+import io.micronaut.starter.fixture.CommandOutputFixture
+import io.micronaut.starter.options.BuildTool
+import io.micronaut.starter.options.Language
+import spock.lang.Unroll
+
+class TurboSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    void 'test readme.md with feature views-turbo contains links to micronaut docs'() {
+        when:
+        Map<String, String> output = generate(['turbo'])
+        String readme = output["README.md"]
+
+        then:
+        readme
+        readme.contains('https://turbo.hotwired.dev/')
+        readme.contains('https://micronaut-projects.github.io/micronaut-views/latest/guide/#turbo')
+    }
+
+    @Unroll
+    void 'test gradle turbo feature for language=#language'() {
+        when:
+        String template = new BuildBuilder(beanContext, BuildTool.GRADLE)
+                .language(language)
+                .features(['turbo'])
+                .render()
+
+        then:
+        template.contains('implementation("io.micronaut.views:micronaut-views-turbo")')
+
+        where:
+        language << Language.values().toList()
+    }
+
+    @Unroll
+    void 'test maven turbo feature for language=#language'() {
+        when:
+        String template = new BuildBuilder(beanContext, BuildTool.MAVEN)
+                .language(language)
+                .features(['turbo'])
+                .render()
+        BuildTestVerifier verifier = BuildTestUtil.verifier(BuildTool.MAVEN, language, template)
+
+        then:
+        verifier.hasDependency('io.micronaut.views', 'micronaut-views-turbo', Scope.COMPILE)
+
+        where:
+        language << Language.values().toList()
+    }
+}
