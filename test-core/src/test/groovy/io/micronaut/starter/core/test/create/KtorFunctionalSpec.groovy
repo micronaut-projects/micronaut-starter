c39068dc8a13cd24c25797f05aba7e13c8bd071f
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-01 08:39:34 +0200

    rename to KtorFunctionalSpec

--- test-core/src/test/groovy/io/micronaut/starter/core/test/create/KtorFunctionalSpec.groovy
+++ test-core/src/test/groovy/io/micronaut/starter/core/test/create/KtorFunctionalSpec.groovy
@@ -4,10 +4,9 @@ import io.micronaut.starter.application.ApplicationType
 import io.micronaut.starter.feature.kotlin.Ktor
 import io.micronaut.starter.options.BuildTool
 import io.micronaut.starter.options.Language
-import io.micronaut.starter.test.BuildToolCombinations
 import io.micronaut.starter.test.CommandSpec
 
-class KtorSpec extends CommandSpec {
+class KtorFunctionalSpec extends CommandSpec {
 
     void 'create-app with feature ktor for #lang and #buildTool starts successfully'(Language lang, BuildTool buildTool) {
         given:
