354fb19f0f835f37224cd9c4530a1e3e6f5c565c
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-16 17:32:40 +0200

    rename to SwaggerUIFunctionalSpec

--- test-features/src/test/groovy/io/micronaut/starter/core/test/feature/other/SwaggerUIFunctionalSpec.groovy
+++ test-features/src/test/groovy/io/micronaut/starter/core/test/feature/other/SwaggerUIFunctionalSpec.groovy
@@ -14,7 +14,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
-class SwaggerUISpec extends CommandSpec {
+class SwaggerUIFunctionalSpec extends CommandSpec {
 
     Path applicationYAMLPath
     Path openAPIPropertiesPath
